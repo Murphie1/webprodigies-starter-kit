ac30ce0b996a735c3eaeead90576a2faf8dbc223
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-19 15:23:48 +0100

    Update initial-profile.ts

--- src/lib/initial-profile.ts
+++ src/lib/initial-profile.ts
@@ -1,17 +1,16 @@
 import { client } from "@/lib/prisma"
-import { RedirectToSignIn } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
 import { currentUser } from "@clerk/nextjs/server"
 
 export const InitialProfile = async () => {
-    const user = await currentUser()
+    const clerk = await currentUser()
 
-    if (!user) {
-        return RedirectToSignIn()
+    if (!clerk) {
+        return redirect("/sign-in")
     }
-    const clerkId = user.id
     const profile = await client.profile.findUnique({
         where: {
-            clerkId: clerkId, // Use clerkId to find the profile
+            clerkId: clerk.id, // Use clerkId to find the profile
         },
     })
 
@@ -21,9 +20,9 @@ export const InitialProfile = async () => {
 
     const newProfile = await client.profile.create({
         data: {
-            clerkId: clerkId, // Use clerkId when creating a new profile
-            name: `${user.firstName}`,
-            imageUrl: user.imageUrl,
+            clerkId: clerk.id, // Use clerkId when creating a new profile
+            name: `${clerk.firstName}`,
+            imageUrl: clerk.imageUrl,
             // email: user.emailAddresses[0]?.emailAddress, // also add to schema profile model
         },
     })
