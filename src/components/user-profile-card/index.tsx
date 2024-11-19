c4bda6514923fe19c0c9b48903cd3b6da745ab7f
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-19 17:33:59 +0100

    Update index.tsx

--- src/components/user-profile-card/index.tsx
+++ src/components/user-profile-card/index.tsx
@@ -12,7 +12,7 @@ import Image from "next/image"
 import { useRouter } from "next/navigation"
 
 interface UserProfileCardProps {
-    imageUrl: string
+    imageUrl: string | null
     name: string
     type: string | null
     email: string | null
