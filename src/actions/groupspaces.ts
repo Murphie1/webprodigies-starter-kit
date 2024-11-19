06fc70187a88d013289c972b0eba487efd7bef32
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-19 15:11:12 +0100

    Update groupspaces.ts

--- src/actions/groupspaces.ts
+++ src/actions/groupspaces.ts
@@ -3,6 +3,6 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 export const getGroupspaces = async () => {
-    const groupspaces = await prisma.groupspaces.findMany()
+    const groupspaces = await prisma.groupspace.findMany()
     return groupspaces
 }
