f0023197c76b3e8e85494da2047e7727211ef281
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-19 15:33:58 +0100

    Update page.tsx2

--- src/app/(profile)/page.tsx
+++ src/app/(profile)/page.tsx
@@ -1,25 +1,35 @@
-import { InitialProfile } from "@/lib/initial-profile"
-import { client } from "@/lib/prisma"
-import { currentUser } from "@clerk/nextjs/server"
-import { redirect } from "next/navigation"
+import { InitialProfile } from "@/lib/initial-profile";
+import { client } from "@/lib/prisma";
+import { InitialUser } from "@/lib/initial-user";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const ProfilePage = async () => {
-    const profile = await InitialProfile()
-    const user = await currentUser()
+    const profile = await InitialProfile();
+    const localUser = await InitialUser();
+    const user = await currentUser();
+
     if (!user) {
-        return redirect("/sign-in")
+        redirect("/sign-in");
     }
-    const clerkId = user.id
+
+    const clerkId = user.id;
     const kidsprofile = await client.kidsProfile.findUnique({
         where: {
             clerkId: clerkId, // Use clerkId to find the profile
         },
-    })
+    });
 
     if (kidsprofile) {
-        return kidsprofile
+        return (
+            <div>
+                <h1>Welcome, {user.firstName}</h1>
+                <p>Profile exists: {JSON.stringify(kidsprofile)}</p>
+            </div>
+        );
     }
 
-    return <div className="justify-end pt-5 pr-5">Create a Kids Profile</div>
-}
-export default ProfilePage
+    return <div className="justify-end pt-5 pr-5">Create a Kids Profile</div>;
+};
+
+export default ProfilePage;
