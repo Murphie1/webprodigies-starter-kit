669b1096142d235bbae13e10c3bdb85326977fd2
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-19 17:54:41 +0100

    Create route.ts

---
+++ src/app/api/profile/route.ts
@@ -0,0 +1,35 @@
+import { onAuthenticatedUser } from "@/actions/auth"
+import { client } from "@/lib/prisma"
+import { currentUser } from "@clerk/nextjs/server"
+import 
+import { redirect } from "next/navigation"
+import { NextResponse } from "next/server"
+import { v4 as uuidv4 } from "uuid"
+
+export async function POST(req: Request) {
+    try {
+        const clerk = await currentUser()
+        if (!clerk) {
+            return redirect("/sign-in")
+        }
+        const { name, imageUrl } = await req.json()
+        const user = await onAuthenticatedUser()
+
+        if (!user) {
+            return new NextResponse("userless", { status: 401 })
+        }
+
+        const profile = await client.profile.create({
+            data: {
+                clerkId: clerk.id,
+                name,
+                imageUrl,
+                email: email: user.emailAddresses[0]?.emailAddress,
+                userId: user.id,
+            },
+        })
+    } catch (error) {
+        console.log("[PROFILES_POST]", error)
+        return new NextResponse("Internal Error", { status: 500 })
+    }
+  }
