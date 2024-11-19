3fa29b62d232978db0e9fa721e2a5b33fa5c37c2
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-19 20:09:58 +0100

    Create profile-bottom.tsx

---
+++ src/app/(profile)/_components/profile-bottom.tsx
@@ -0,0 +1,82 @@
+import react from "react"
+
+
+export function ProfileBottom() {
+    return (
+      <div>
+        <h4>
+        Starter tools
+        </h4>
+<div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+                <a
+                    href="/about"
+                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2 className="mb-3 text-2xl font-semibold">
+                        About{" "}
+                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                            -&gt;
+                        </span>
+                    </h2>
+                    <p className="m-0 max-w-[30ch] text-sm opacity-50">
+                        Find in-depth information about YouLearn&apos;s features and
+                        services.
+                    </p>
+                </a>
+
+                <a
+                    href="/plans"
+                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2 className="mb-3 text-2xl font-semibold">
+                        Plans{" "}
+                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                            -&gt;
+                        </span>
+                    </h2>
+                    <p className="m-0 max-w-[30ch] text-sm opacity-50">
+                        Check out our flexible and select the right one for you. Now worries, whatever your budget is, we'll fit right in. We've got something for everyone.
+                    </p>
+                </a>
+
+                <a
+                    href="/launchpad"
+                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2 className="mb-3 text-2xl font-semibold">
+                        Onboarding{" "}
+                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                            -&gt;
+                        </span>
+                    </h2>
+                    <p className="m-0 max-w-[30ch] text-sm opacity-50">
+                        Learn all about the platform, it's features and how to navigate before diving it. Don&apos;t worry, we've got a similar in-app feature im case you want to dive right in. Just look out for the Launchpad in 'More', or use tge the feature finder on the top navigation bar👀.
+                    </p>
+                </a>
+
+                <a
+                    href="/terms"
+                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2 className="mb-3 text-2xl font-semibold">
+                        Terms and Policies{" "}
+                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                            -&gt;
+                        </span>
+                    </h2>
+                    <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+                   Learn all about our terms, conditions, privacy policies, how we handle user data and more. Be sure to read it, dont just skip it like we do other times😅.
+                    </p>
+                </a>
+            </div>
+        </div>
+)
+}
