d81ced432a3575ec1d0f542327fb6b6588c73fc1
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-19 20:16:23 +0100

    Update profile-comp.tsx

--- src/app/(profile)/_components/profile-comp.tsx
+++ src/app/(profile)/_components/profile-comp.tsx
@@ -3,13 +3,14 @@ import { client } from "@/lib/prisma";
 import { InitialUser } from "@/lib/initial-user";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import { ProfileBottom } from "./profile-bottom"
 
 const ProfileComp = async () => {
     const profile = await InitialProfile();
     const localUser = await InitialUser();
 
-    return <ProfileBottom />
+    return <h5>
+    Profiles
+    </h5>
 };
 
 export default ProfileComp
