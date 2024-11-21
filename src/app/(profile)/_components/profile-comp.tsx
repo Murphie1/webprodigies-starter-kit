70f79e69b1abde475b60787f106164a4c2d760b1
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-21 15:37:26 +0100

    Update profile-comp.tsx

--- src/app/(profile)/_components/profile-comp.tsx
+++ src/app/(profile)/_components/profile-comp.tsx
@@ -1,22 +1,25 @@
 import { InitialProfile } from "@/lib/initial-profile";
 import { ProfileCard } from "@/components/user-profile-card";
-    import { CreateProfile} from "@/components/modals/create-profile";
+import { CreateProfile } from "@/components/modals/create-profile";
 
 const ProfileComp = async () => {
     const profile = await InitialProfile();
-   if (profile) {
-    return 
-<div className="flex overflow-x-auto whitespace-nowrap w-full">
-                <h2>Continue with another Profile </h2>
+
+    if (profile) {
+        return (
+            <div className="flex overflow-x-auto whitespace-nowrap w-full">
+                <h2>Continue with another Profile</h2>
                 <ProfileCard
                     name={profile.name}
                     imageUrl={profile.imageUrl || null}
                     type={profile.type || null}
                     email={profile.email || null}
                 />
-            </div>;
-}
-return <CreateProfile />;
+            </div>
+        );
+    }
+
+    return <CreateProfile />;
 };
 
-export default ProfileComp
+export default ProfileComp;
