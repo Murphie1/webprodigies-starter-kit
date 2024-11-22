f173f0ee439c7569e26feee5187a6444b99f7050
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-22 15:20:13 +0100

    Update globe-duotone-black.tsx

--- src/icons/globe-duotone-black.tsx
+++ src/icons/globe-duotone-black.tsx
@@ -10,7 +10,7 @@ export const GlobeDuoToneBlack = () => {
             stroke-width="1.25"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="lucide lucide-globe"
+            className="lucide lucide-globe"
         >
             <circle cx="12" cy="12" r="10" />
             <path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20" />
