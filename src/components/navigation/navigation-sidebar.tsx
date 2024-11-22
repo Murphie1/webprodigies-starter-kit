cdc74c3ea218e6f0b65b8ec0887f0e1a61d9e812
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-22 16:15:56 +0100

    Update navigation-sidebar.tsx

--- src/components/navigation/navigation-sidebar.tsx
+++ src/components/navigation/navigation-sidebar.tsx
@@ -41,6 +41,7 @@ export const NavigationSideBar = async ({
             },
         },
     })
+    if (groupspaces) {
     return (
         <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22]">
             <NavigationAction />
@@ -69,4 +70,6 @@ export const NavigationSideBar = async ({
             </div>
         </div>
     )
+}
+    return null
 }
