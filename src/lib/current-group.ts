fccd9c1a5d845cab2a48c03b7ac8a1274407145e
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-22 15:35:38 +0100

    Update current-group.ts

--- src/lib/current-group.ts
+++ src/lib/current-group.ts
@@ -3,10 +3,10 @@ import { client } from "@/lib/prisma"
 
 export const currentGroup = async () => {
     const user = await onAuthenticatedUser()
-    const group = await client.group.findUnique({
+    const group = await client.group.findMany({
         where: {
             userId: user.id,
         },
     })
-    return profile
+    return group
 }
