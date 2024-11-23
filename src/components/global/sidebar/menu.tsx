b90f59543273475f85032bb9c9aeeea937579b8d
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-23 09:57:27 +0100

    Update menu.tsx

--- src/components/global/sidebar/menu.tsx
+++ src/components/global/sidebar/menu.tsx
@@ -9,6 +9,8 @@ import { usePathname } from "next/navigation"
 import { IChannels } from "."
 import { IconRenderer } from "../icon-renderer"
 import IconDropDown from "./icon-dropdown"
+import { NavigationSideBar } from "@/components/navigation/navigation-sidebar"
+
 
 type Props = {
     channels: IChannels[]
