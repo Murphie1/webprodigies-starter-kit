64762a99e50a1e9eb0f5b74396ecdb9ed7d11cc0
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-24 22:09:18 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,7 @@ const config = {
             padding: "2rem",
             screens: {
                 "2xl": "1400px",
+                xs: '360px',
             },
         },
         backgroundImage: {
@@ -26,6 +27,23 @@ const config = {
                 sans: ["var(--font-inter)"],
             },
             colors: {
+                blue: {
+          100: '#B4C6EE',
+          400: '#417BFF',
+          500: '#3371FF',
+        },
+        red: {
+          400: '#DD4F56',
+          500: '#DC4349',
+        },
+                dark: {
+          100: '#09111F',
+          200: '#0B1527',
+          300: '#0F1C34',
+          350: '#12213B',
+          400: '#27344D',
+          500: '#2E3D5B',
+        },
                 themeBlack: "#09090B",
                 themeGray: "#27272A",
                 themeDarkGray: "#27272A",
@@ -80,6 +98,10 @@ const config = {
                     to: { height: "0" },
                 },
             },
+            backgroundImage: {
+        doc: 'url(/assets/images/doc.png)',
+        modal: 'url(/assets/images/modal.png)',
+      },
             animation: {
                 "accordion-down": "accordion-down 0.2s ease-out",
                 "accordion-up": "accordion-up 0.2s ease-out",
