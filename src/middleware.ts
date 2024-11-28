6c1c6335a4c23ce778e5af5be4b7880b959322a9
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-28 14:57:32 +0100

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
-import { NextResponse } from "next/server"
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/sign-in(.*)"])
-
-export default clerkMiddleware(async (auth, req) => {
-    const baseHost = "localhost:3000"
-    const host = req.headers.get("host")
-    const reqPath = req.nextUrl.pathname
-    const origin = req.nextUrl.origin
-    if (!isProtectedRoute(req)) auth().protect()
-    // if (!baseHost.includes(host as string) && reqPath.includes("/organizations")) {
-    // const response = await fetch(`${origin}/api/domain?host=${host}`, {
-    // method: "GET",
-    //headers: {
-    //   "Content-Type": "application/json",
-    //  },
-    //   })
-
-    //  const data = await response.json()
-    //  if (data.status === 200 && data) {
-    // return NextResponse.rewrite(
-    //  new URL(reqPath, `https://${data.domain}/${reqPath}`),
-    // )
-    // }
-    // }
-})
+export default clerkMiddleware();
 
 export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-}
+  matcher: ["/((?!.*\\..*|_next).*)", "/"], // Matches all pages except static assets.
+};
