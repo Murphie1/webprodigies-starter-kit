30abbaca07997dc7547260941bfafe7cdaa4de3d
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-28 15:54:22 +0100

    Update page.tsx

--- src/app/(profile)/page.tsx
+++ src/app/(profile)/page.tsx
@@ -7,8 +7,8 @@ import ProfileTop from "./_components/profile-top";
 
 const Home = () => {
 
-  return <div className="flex flex-col items-center space-y-5 h-auto min-h-screen justify-center">
-  <ProfileTop />
+  return <div className="flex flex-col items-center space-y-3 h-auto min-h-screen justify-center">
+ <div className="pb-5"><ProfileTop /></div>
     <UserComp />
       <div className="overflow-x-auto whitespace-nowrap">
   <ProfileComp />
