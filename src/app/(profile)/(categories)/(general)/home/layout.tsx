440dc0f57ef978bc4fdc7287d02294562cae8de5
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-29 16:54:08 +0100

    Create layout.tsx

---
+++ src/app/(profile)/(categories)/(general)/home/layout.tsx
@@ -0,0 +1,21 @@
+//Home layout
+import { NavBar } from "@/components/navbar";
+import { SideBar } from "@/components/sidebar";
+
+const HomeLayout  = ({
+children
+}: {
+children: React.ReactNode;
+}) => {
+return (
+<div className="h-full">
+<div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
+<NavBar />
+</div>
+<div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+<SideBar />
+</div>
+<main className="md:pl-56 h-full pt-[80px]">
+{children}
+</main>
+</div>
