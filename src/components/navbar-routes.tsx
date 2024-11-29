df5ff0fbc047809ddb3cd08d90fc0619ecbcd42b
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-29 17:02:57 +0100

    Create navbar-routes.tsx

---
+++ src/components/navbar-routes.tsx
@@ -0,0 +1,29 @@
+//navbar-routes
+import { usePathname } from "next/navigation";
+import Link from "next/Link";
+import { UserButton } from "@clerk/nextjs";
+import { LogOut } from "lucide-react";
+import { ModeToggle } from "./mode-toggle";
+import { Button } from "@/components/ui/button";
+
+
+export const NavbarRoutes = () => {
+const pathname = usePathname();
+const isTeacherPage = pathname?.startsWith("/teacher");
+const isPlayerPage = pathname?.includes("/chapter");
+
+return (
+<div className="flex gap-x-2 ml-auto">
+{isTeacherPage || isPlayerPage ? (
+<Link href="/home">
+<Button className="size-sm" variant="ghost">
+<LogOut />
+Exit
+</Button>
+</Link>) : ( 
+<ModeToggle />
+)}
+<UserButton 
+afterSignOutUrl="/sign-in"
+/>
+</div>
