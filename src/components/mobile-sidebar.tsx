63d909b578ce148a95df13ae5a4ffad51f2071fe
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-29 17:02:14 +0100

    Create mobile-sidebar.tsx

---
+++ src/components/mobile-sidebar.tsx
@@ -0,0 +1,21 @@
+//mobile-sidebar 
+import { Menu } from "lucide-react";
+import {
+Sheet
+SheetTrigger
+SheetContent
+} from "@/components/ui/sheet";
+import { SideBar } from "./sidebar";
+
+export const MobileSideBar = () => {
+return (
+<Sheet>
+<SheetTrigger className="md:hidden pr:4 hover:opacity-75 transition">
+<Menu />
+</SheetTrigger>
+<SheetContent>
+<SideBar />
+</SheetContent>
+</Sheet>
+)
+}
