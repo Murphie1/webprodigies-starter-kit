6a8a831eccb7794d84f8ae9d8e67d759491c8ca0
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-29 18:13:19 +0100

    Update layout.tsx

--- src/app/(profile)/(categories)/(general)/home/layout.tsx
+++ src/app/(profile)/(categories)/(general)/home/layout.tsx
@@ -3,22 +3,22 @@ import { SideBar } from "@/components/sidebar";
 
 const HomeLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="h-full">
-      {/* Navbar */}
-      <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
-        <NavBar />
-      </div>
+   // <div className="h-full">
+    //  {/* Navbar */}
+      //<div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
+      //  <NavBar />
+      //</div>
 
-      {/* Sidebar */}
-      <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
-        <SideBar />
-      </div>
+     // {/* Sidebar */}
+     // <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+    //    <SideBar />
+     // </div>
 
       {/* Main Content */}
       <main className="md:pl-56 h-full pt-[80px]">
         {children}
       </main>
-    </div>
+   // </div>
   );
 };
 
