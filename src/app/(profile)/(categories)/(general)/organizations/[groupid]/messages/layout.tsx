992fc607c1479215fe1a4932f11fcc0e613d9686
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-30 10:48:37 +0100

    Update layout.tsx

--- src/app/(profile)/(categories)/(general)/organizations/[groupid]/messages/layout.tsx
+++ src/app/(profile)/(categories)/(general)/organizations/[groupid]/messages/layout.tsx
@@ -1,6 +1,10 @@
 import GlassSheet from "@/components/global/glass-sheet"
 import { Menu } from "lucide-react"
 import { GroupChatMenu } from "./_components/chat-menu"
+import SideBar from "@/components/global/sidebar"
+import { onAuthenticatedUser } from "@/actions/auth"
+import { redirect } from "next/navigation"
+
 
 type HuddlesLayoutProps = {
     children: React.ReactNode
@@ -8,10 +12,16 @@ type HuddlesLayoutProps = {
 }
 
 const HuddlesLayout = async ({ children, params }: HuddlesLayoutProps) => {
+    const user = await onAuthenticatedUser()
+    if (!user.id) redirect("/sign-in")
+    
     return (
         <div className="grid grid-cols-1 lg:grid-cols-6 flex-1 h-0">
             <div className="lg:col-span-4 flex flex-col">
                 <div className="flex justify-between items-center p-5 lg:hidden">
+                    <GlassSheet trigger={<Menu />}>
+                        <SideBar groupid={params.groupid} userid={user.id} />
+                    </GlassSheet>
                     <p className="font-medium text-gray-300 dark:text-themeTextWhite">
                         No chat selected
                     </p>
