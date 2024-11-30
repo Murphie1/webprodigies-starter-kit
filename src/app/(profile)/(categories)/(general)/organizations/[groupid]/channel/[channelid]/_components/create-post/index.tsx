e785342486da12b13d14a21305a46fde65923693
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-30 12:08:17 +0100

    Update index.tsx

--- src/app/(profile)/(categories)/(general)/organizations/[groupid]/channel/[channelid]/_components/create-post/index.tsx
+++ src/app/(profile)/(categories)/(general)/organizations/[groupid]/channel/[channelid]/_components/create-post/index.tsx
@@ -18,14 +18,14 @@ const CreateNewPost = ({ channelid, userImage, username }: Props) => {
             <SimpleModal
                 trigger={
                     <span>
-                        <Card className="border-themeGray cursor-pointer first-letter:rounded-2xl overflow-hidden">
-                            <CardContent className="p-3 bg-[#1A1A1D] flex gap-x-6 items-center ">
+                        <Card className="border-black dark:border-themeGray cursor-pointer first-letter:rounded-2xl overflow-hidden">
+                            <CardContent className="p-3 bg-white dark:bg-[#1A1A1D] flex gap-x-6 items-center ">
                                 <Avatar className="cursor-pointer">
                                     <AvatarImage src={userImage} alt="user" />
                                     <AvatarFallback>U</AvatarFallback>
                                 </Avatar>
-                                <CardDescription className="text-themeTextGray">
-                                    Type / to add elements to your post...
+                                <CardDescription className="text-black dark:text-themeTextGray">
+                                    Click here to create a post.
                                 </CardDescription>
                             </CardContent>
                         </Card>
@@ -38,12 +38,12 @@ const CreateNewPost = ({ channelid, userImage, username }: Props) => {
                         <AvatarFallback>U</AvatarFallback>
                     </Avatar>
                     <div className="flex flex-col">
-                        <p className="text-themeTextGray text-sm capitalize">
+                        <p className="text-black dark:text-themeTextGray text-sm capitalize">
                             {username}
                         </p>
-                        <p className="text-sm captialize text-themeTextGray">
+                        <p className="text-sm captialize text-black dark:text-themeTextGray">
                             Posting in{" "}
-                            <span className="font-bold capitalize text-themeTextWhite">
+                            <span className="font-bold capitalize text-themeTextBlack dark:text-themeTextWhite">
                                 {name}
                             </span>
                         </p>
