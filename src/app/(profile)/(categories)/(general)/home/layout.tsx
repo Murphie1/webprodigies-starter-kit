7447b9817f822d7bf2ce9b287e442382ca46ccae
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-30 08:56:01 +0100

    Update layout.tsx

--- src/app/(profile)/(categories)/(general)/home/layout.tsx
+++ src/app/(profile)/(categories)/(general)/home/layout.tsx
@@ -1,5 +1,5 @@
 import { NavBar } from "@/components/navbar";
-// import { SideBar } from "@/components/sidebar";
+import { SideBar } from "@/components/sidebar";
 
 type Props = {
   children: React.ReactNode;
@@ -9,17 +9,17 @@ const HomeLayout = ({ children }: Props) => {
   return (
     <div className="h-full">
       {/* Navbar */}
-       <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
+       <div className="h-[70px] md:pl-56 fixed inset-y-0 w-full z-50">
         <NavBar />
       </div> 
 
       {/* Sidebar */}
-      {/* <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+       <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
         <SideBar />
-      </div> */}
+      </div> 
 
       {/* Main Content */}
-      <main className="h-full">{children}</main>
+      <main className="h-full md:pl-56 pt-[70px]">{children}</main>
     </div>
   );
 };
