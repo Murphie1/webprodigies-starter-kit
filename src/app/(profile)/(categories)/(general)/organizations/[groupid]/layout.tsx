4a98c574864331bec710ec2863694b0c12a93122
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-30 09:20:41 +0100

    Update layout.tsx

--- src/app/(profile)/(categories)/(general)/organizations/[groupid]/layout.tsx
+++ src/app/(profile)/(categories)/(general)/organizations/[groupid]/layout.tsx
@@ -70,7 +70,7 @@ const GroupLayout = async ({ children, params }: Props) => {
             <div className="flex h-screen md:pt-5">
                 <SideBar groupid={params.groupid} userid={user.id} />
                 <div className="md:ml-[300px] flex flex-col flex-1 bg-[#101011] md:rounded-tl-xl overflow-y-auto border-l-[1px] border-t-[1px] border-[#28282D]">
-                    <Navbar groupid={params.groupid} userid={user.id} />
+        {/*  <Navbar groupid={params.groupid} userid={user.id} /> */}
                     {children}
                     <div className="fixed pl-1 bottom-10 pb-10 md:hidden">
                         <Sheet>
