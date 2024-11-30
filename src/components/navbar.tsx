8b6652a383d04ff34356ea966a87e7b56d35e7e1
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-30 08:13:49 +0100

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,12 @@
 //NavBar
 
 import { NavbarRoutes } from "./navbar-routes";
-import { MobileSideBar } from "./mobile-sidebar";
+//import { MobileSideBar } from "./mobile-sidebar";
 
 export const NavBar = () => {
 return (
 <div className="h-full p-4 flex items-center bg-white shadow-sm dark:bg-black">
-<MobileSideBar />
+  {/*<MobileSideBar />*/}
 <NavbarRoutes />
 </div>
 )
