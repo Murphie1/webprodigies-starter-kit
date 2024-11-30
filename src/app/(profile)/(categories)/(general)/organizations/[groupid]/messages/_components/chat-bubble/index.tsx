324bb33275668d25ba25041d70286954b40ede00
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-30 10:32:38 +0100

    Update index.tsx

--- src/app/(profile)/(categories)/(general)/organizations/[groupid]/messages/_components/chat-bubble/index.tsx
+++ src/app/(profile)/(categories)/(general)/organizations/[groupid]/messages/_components/chat-bubble/index.tsx
@@ -17,13 +17,13 @@ export const ChatBubble = ({
         <div
             className={cn(
                 senderid === userid
-                    ? "self-end bg-themeBlack max-w-[60%] min-w-[15%]"
-                    : "self-start bg-themeGray max-w-[60%] min-w-[15%]",
+                    ? "self-end bg-black text-white dark:bg-themeBlack max-w-[60%] min-w-[15%]"
+                    : "self-start bg-sky-100 dark:bg-themeGray max-w-[60%] min-w-[15%]",
                 "px-4 py-2 rounded-xl text-xl flex flex-col",
             )}
         >
             <p>{message}</p>
-            <p className={cn("text-xs text-themeTextGray")}>
+            <p className={cn("text-xs text-gray-100 dark:text-themeTextGray")}>
                 {createdAt && (
                     <>
                         {createdAt.getHours()} {createdAt.getMinutes()}{" "}
