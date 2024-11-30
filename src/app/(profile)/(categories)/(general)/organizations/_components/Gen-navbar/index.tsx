5ed7273ab61024b9ece0bfd9434802e52dec7ab1
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-30 13:01:58 +0100

    Update index.tsx

--- src/app/(profile)/(categories)/(general)/organizations/_components/Gen-navbar/index.tsx
+++ src/app/(profile)/(categories)/(general)/organizations/_components/Gen-navbar/index.tsx
@@ -7,9 +7,9 @@ const OrganizationNavbar = (props: Props) => {
         <div className="pb-2 pt-0 bg-black">
             <div className="w-full flex justify-between sticky top-0 items-center z-50 pr-2 fixed overflow-x-auto whitespace-nowrap">
                 <p className="font-bold text-2xl pl-2">Syncro</p>
-                <div className="md:hidden">
+                {/*<div className="md:hidden">
                     <Menu orientation="mobile" />
-                </div>
+                </div>*/}
                 <Menu orientation="desktop" />
             </div>
         </div>
