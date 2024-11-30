5e5a2a561152c64880d9fcdacdfa375401dbddeb
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-30 10:18:18 +0100

    Update page.tsx

--- src/app/(profile)/(categories)/(general)/organizations/[groupid]/settings/affiliates/page.tsx
+++ src/app/(profile)/(categories)/(general)/organizations/[groupid]/settings/affiliates/page.tsx
@@ -10,20 +10,20 @@ const Affiliate = async ({ params }: Props) => {
     const affiliate = await onGetAffiliateLink(params.groupid)
     return (
         <div className="flex flex-col items-start p-5">
-            <Card className="border-themeGray bg-[#1A1A1D] p-5">
+            <Card className="border-black dark:border-themeGray bg-white dark:bg-[#1A1A1D] p-5">
                 <CardTitle className="text-3xl">Affiliate Link</CardTitle>
-                <CardDescription className="text-themeTextGray">
+                <CardDescription className="text-gray dark:text-themeTextGray">
                     Create and share an invitations link
                 </CardDescription>
                 <div className="mt-8 flex flex-col gap-y-2">
-                    <div className="bg-black border-themeGray p-3 rounded-lg flex gap-x-5 items-center">
-                        http://localhost:3000/affiliates/
+                    <div className="bg-white dark:bg-black border-black dark:border-themeGray p-3 rounded-lg flex gap-x-5 items-center">
+                        https://ylttest.vercel.app/affiliates/
                         {affiliate.affiliate?.id}
                         <CopyButton
-                            content={`https://mxls3q-3000.csb.app/${affiliate.affiliate?.id}`}
+                            content={`https://ylttest.vercel.app/${affiliate.affiliate?.id}`}
                         />
                     </div>
-                    <CardDescription className="text-themeTextGray">
+                    <CardDescription className="text-gray dark:text-themeTextGray">
                         This link will redirect users to the main page where{" "}
                         <br />
                         they can purchase or request memberships
