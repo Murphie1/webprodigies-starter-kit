6712c267d518c8944d2786e4cd5b87ca4b3610da
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-30 09:52:50 +0100

    Update page.tsx

--- src/app/(profile)/(categories)/(general)/organizations/[groupid]/settings/page.tsx
+++ src/app/(profile)/(categories)/(general)/organizations/[groupid]/settings/page.tsx
@@ -7,7 +7,7 @@ const GroupSettingsPage = ({ params }: { params: { groupid: string } }) => {
         <div className="flex flex-col w-full h-full gap-10 px-16 py-10 overflow-auto">
             <div className="flex flex-col">
                 <h3 className="text-3xl font-bold">Organization Settings</h3>
-                <p className="text-sm text-themeTextGray">
+                <p className="text-sm text-black dark:text-themeTextGray">
                     Adjust your settings here. These settings might take time to
                     reflect on the explore page.
                 </p>
