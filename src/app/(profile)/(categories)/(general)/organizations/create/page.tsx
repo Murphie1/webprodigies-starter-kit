66b172bb82dc3d6bb81f859e446b12969f9588dc
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-30 13:21:45 +0100

    Update page.tsx

--- src/app/(profile)/(categories)/(general)/organizations/create/page.tsx
+++ src/app/(profile)/(categories)/(general)/organizations/create/page.tsx
@@ -25,7 +25,7 @@ const GroupCreatePage = async ({
                 </h5>
                 <Link href="/stripe.com">
                 <p className="text-black hover:text-sky-400 text-sm leading-tight dark:text-themeWhite">
-                    Or pay on Stripe's checkout website for Maximum Security
+                    Or pay on Stripe&apos;s checkout website for Maximum Security
                 </p>
                     </Link>
                 {affiliate.status === 200 && (
