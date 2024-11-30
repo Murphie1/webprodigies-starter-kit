556f4e6e89e77c82015c11a4977a62c139db166b
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-30 11:29:18 +0100

    Update page.tsx

--- src/app/(profile)/(categories)/(general)/organizations/[groupid]/courses/page.tsx
+++ src/app/(profile)/(categories)/(general)/organizations/[groupid]/courses/page.tsx
@@ -6,6 +6,11 @@ import {
     dehydrate,
 } from "@tanstack/react-query"
 import CourseList from "./_components/course-list"
+import GlassSheet from "@/components/global/glass-sheet"
+import { Menu } from "lucide-react"
+import SideBar from "@/components/global/sidebar"
+import { onAuthenticatedUser } from "@/actions/auth"
+import { redirect } from "next/navigation"
 
 type Props = {
     params: {
@@ -15,6 +20,8 @@ type Props = {
 
 const CoursesPage = async ({ params }: Props) => {
     const client = new QueryClient()
+    const user = await onAuthenticatedUser()
+    if (!user.id) redirect("/sign-in")
 
     await client.prefetchQuery({
         queryKey: ["group-courses"],
@@ -23,10 +30,17 @@ const CoursesPage = async ({ params }: Props) => {
 
     return (
         <HydrationBoundary state={dehydrate(client)}>
+            <div className="relative">
+                <div className="flex justify-start items-center p-5 md:hidden">
+                    <GlassSheet trigger={<Menu />} className="md:hidden cursor-pointer" >
+                        <SideBar groupid={params.groupid} userid={user.id} mobile />
+                    </GlassSheet>
+                </div>
             <div className="container grid lg:grid-cols-2 2xl:grid-cols-3 py-10 gap-5">
                 <CourseCreate groupid={params.groupid} />
                 <CourseList groupid={params.groupid} />
             </div>
+                </div>
         </HydrationBoundary>
     )
 }
