5d9d73dc59b04901c920aace85b6438e49a8ddd4
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-11-30 11:16:58 +0100

    Update index.tsx

--- src/app/(profile)/(categories)/(general)/organizations/_components/navbar/index.tsx
+++ src/app/(profile)/(categories)/(general)/organizations/_components/navbar/index.tsx
@@ -19,27 +19,21 @@ type NavbarProps = {
 
 export const Navbar = async ({ groupid, userid }: NavbarProps) => {
     const user = await onAuthenticatedUser()//currentUser()
-   // const pathname = usePathname()
-   // const currentPage = pathname.split("/").pop()
-
-   // if (pathname.includes("groupspaces")) {
-      //  return null
-   // }
-    
+   
     return (
-        <div className="bg-[#1A1A1D] py-2 px-3 md:px-7 md:py-5 flex gap-5 justify-between md:justify-end items-center">
+        <div className="bg-white dark:bg-[#1A1A1D] py-2 px-3 md:px-7 md:py-5 flex gap-5 justify-between md:justify-end items-center">
             <GlassSheet trigger={<Menu className="md:hidden cursor-pointer" />}>
                 <SideBar groupid={groupid} userid={userid} mobile />
             </GlassSheet>
             <Search
                 searchType="POSTS"
-                className="rounded-full border-themeGray bg-black !opacity-100 px-3"
+                className="rounded-full border-black dark:border-themeGray bg-white dark:bg-black !opacity-100 px-3"
                 placeholder="Search..."
             />
             <Link href={`/organizations/create`} className="hidden md:inline">
                 <Button
                     variant="outline"
-                    className="bg-themeBlack rounded-2xl flex gap-2 border-themeGray hover:bg-themeGray"
+                    className="bg-white dark:bg-themeBlack rounded-2xl flex gap-2 border-black dark:border-themeGray hover:bg-themeGray"
                 >
                     <CirclePlus />
                     Create Group
