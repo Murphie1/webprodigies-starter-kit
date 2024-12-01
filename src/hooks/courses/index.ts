911de9265bb3e4334c7a89cd113ed879cc5257c5
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-01 23:08:53 +0100

    Update index.ts

--- src/hooks/courses/index.ts
+++ src/hooks/courses/index.ts
@@ -65,7 +65,10 @@ export const useCreateCourse = (groupid: string) => {
             privacy: string
             published: boolean
         }) => {
-            const uploaded = await upload.uploadFile(data.image[0])
+            const uploaded = data.image && data.image.length > 0 
+    ? await upload.uploadFile(data.image[0]) 
+    : "https://via.placeholder.com/150"; // Replace with your fallback URL
+            //const uploaded = await upload.uploadFile(data.image[0])
             const course = await onCreateGroupCourse(
                 groupid,
                 data.name,
