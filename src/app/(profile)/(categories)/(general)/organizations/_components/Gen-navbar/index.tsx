120b4871d6b6fda507500bf1f7d602134b2c46e7
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-01 09:58:04 +0100

    Update index.tsx

--- src/app/(profile)/(categories)/(general)/organizations/_components/Gen-navbar/index.tsx
+++ src/app/(profile)/(categories)/(general)/organizations/_components/Gen-navbar/index.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const OrganizationNavbar = (props: Props) => {
     return (
-        <div className="pb-2 pt-0 bg-black">
+        <div className="pb-5 pt-0 bg-white dark:bg-black">
             <div className="w-full flex justify-between sticky top-0 items-center z-50 pr-2 fixed overflow-x-auto whitespace-nowrap">
                 <p className="font-bold text-2xl pl-2">Syncro</p>
                 {/*<div className="md:hidden">
