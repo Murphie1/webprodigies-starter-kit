0fb72f901544ec1e906cebefe1f66a31100ec01b
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-01 19:18:50 +0100

    Update list-item.tsx

--- src/components/global/group-list-slider/list-item.tsx
+++ src/components/global/group-list-slider/list-item.tsx
@@ -16,10 +16,10 @@ export const GroupListItem = ({
     return (
         <div
             className={cn(
-                "flex  gap-3 items-center py-2 px-4 rounded-2xl bg-themeGray border-2 cursor-pointer",
+                "flex  gap-3 items-center py-2 px-4 rounded-2xl bg-transparent dark:bg-themeGray border-2 cursor-pointer",
                 selected === label
-                    ? "border-themeTextGray"
-                    : "border-themeGray",
+                    ? "border-black dark:border-white"
+                    : "border-gray-500 dark:border-themeGray",
             )}
         >
             {icon}
