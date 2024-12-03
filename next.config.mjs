0b440e42fdfa9edffafea9501c6ae78f4ce37009
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-03 08:54:33 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,14 @@ const nextConfig = {
                 protocol: "https",
                 hostname: "img.clerk.com",
             },
+      {
+        protocol: 'https',
+        hostname: 'lovely-flamingo-139.convex.cloud',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sleek-capybara-771.convex.cloud',
+      },
         ],
         domains: ["uploadthing.com"],
     },
