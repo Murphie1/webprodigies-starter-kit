d46ca1240692a76d5a964ff7f8ee07b907f2a050
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-03 21:36:35 +0100

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,6 +18,7 @@ const buttonVariants = cva(
                 secondary:
                     "bg-secondary text-secondary-foreground hover:bg-secondary/80",
                 ghost: "hover:bg-accent hover:text-accent-foreground",
+                plain: "hover:bg-accent hover:text-accent-foreground",
                 link: "text-primary underline-offset-4 hover:underline",
                 primary: "bg-indigo-100 hover:bg-grey-700/50 hover:text-black",
             },
