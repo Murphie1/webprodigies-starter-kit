a903cf61c9581b46e351b58c26150731aac502d8
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-04 17:48:59 +0100

    Update podcasts.tsx

--- src/app/(profile)/(categories)/(general)/home/_components/podcasts.tsx
+++ src/app/(profile)/(categories)/(general)/home/_components/podcasts.tsx
@@ -1,7 +1,7 @@
 "use client"
 import PodcastCard from "@/components/PodcastCard"
 import { useQuery } from "convex/react"
-import { api } from "@/convex/_generated/api"
+import { api } from "~/convex/_generated/api"
 import LoaderSpinner from "@/components/LoaderSpinner"
 
 const Podcasts = () => {
