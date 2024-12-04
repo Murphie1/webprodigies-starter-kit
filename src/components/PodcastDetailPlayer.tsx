3414aaf2bced9a407f1a11a369fb437b02d60486
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-04 09:28:45 +0100

    Update PodcastDetailPlayer.tsx

--- src/components/PodcastDetailPlayer.tsx
+++ src/components/PodcastDetailPlayer.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
 
-import { api } from "@/convex/_generated/api"
+import { api } from "~/convex/_generated/api"
 import { useAudio } from "@/providers/AudioProvider"
 import { PodcastDetailPlayerProps } from "@/type"
 
