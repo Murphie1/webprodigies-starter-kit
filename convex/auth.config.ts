e0136ceff7011337e4454631cfcfde9857f9e281
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-04 07:51:00 +0100

    Update auth.config.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 const authConfig = {
   providers: [
     {
-      domain: "https://fond-sawfly-54.clerk.accounts.dev",
+      domain: "https://helped-stinkbug-75.clerk.accounts.dev",
       applicationID: "convex",
     },
   ]
