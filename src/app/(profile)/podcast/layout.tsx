933f92f4c3f2e06d3472a3aed1b465f7c12015e4
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-05 16:33:23 +0100

    Update layout.tsx

--- src/app/(profile)/podcast/layout.tsx
+++ src/app/(profile)/podcast/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <main className="relative flex flex-col mx-auto pb-5 max-w-(calc[100wh-80px])">
+      <main className="relative flex flex-col mx-auto pb-5 max-w-(calc[100hw-80px])">
              <AudioProvider>
                <Toaster />
                {children}
