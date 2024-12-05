7e302da391b1425af96a554c7c05b2a3f76d7f21
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-05 15:46:27 +0100

    Update layout.tsx

--- src/app/(profile)/podcast/layout.tsx
+++ src/app/(profile)/podcast/layout.tsx
@@ -1,5 +1,5 @@
-import LeftSidebar from "@/components/LeftSidebar";
-import MobileNav from "@/components/MobileNav";
+//import LeftSidebar from "@/components/LeftSidebar";
+//import MobileNav from "@/components/MobileNav";
 //import RightSidebar from "@/components/RightSidebar";
 //import { Toaster } from "@/components/ui/toaster"
 //import PodcastPlayer from "@/components/PodcastPlayer";
@@ -10,26 +10,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="relative flex flex-col">
-      <main className="relative flex">
-        <LeftSidebar />
+    //<div className="relative flex flex-col">
+      <main className="relative flex flex-col">
+        {/*<LeftSidebar />*/}
         
-        <section className="flex min-h-screen flex-1 flex-col px-4 sm:px-14">
+        {/*<section className="flex min-h-screen flex-1 flex-col px-4 sm:px-14">
           <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
             <div className="flex h-16 items-center justify-between md:hidden">
               <MobileNav />
             </div>
             <div className="flex flex-col md:pb-14">
-              {/* <Toaster />*/}
-              {children}
-            </div>
-          </div>
-        </section>
+              <Toaster />*/}
+             <AudioProvider>{children}</AudioProvider>
+        {/*</div>
+          </div>*/}
+          {/* </section>*/}
 
         {/* <RightSidebar />*/}
       </main>
 
       {/* <PodcastPlayer />*/}
-    </div>
+    //</div>
   );
 };
