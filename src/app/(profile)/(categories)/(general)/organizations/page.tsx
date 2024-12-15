93d157d0948558610cc78409cc9e61069da15ab3
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-15 20:36:12 +0100

    Update page.tsx

--- src/app/(profile)/(categories)/(general)/organizations/page.tsx
+++ src/app/(profile)/(categories)/(general)/organizations/page.tsx
@@ -1,7 +1,41 @@
-type Props = {}
+import { client } from "@/lib/prisma"
+import GroupBox from "./_components/group-box"
+import { onAuthenticatedUser } from "@/actions/auth"
+import { redirect } from "next/navigation"
 
-const Orgs = () => {
-    return <div>Wow</div>
-}
+
+const Orgs = async () => {
+    
+const user = await onAuthenticatedUser()
+if (!user.id) return redirect("/sign-in")
+
+
+const group = await client.group.findMany({
+        where: {
+            userId: user.id, // Use clerkId to find all profiles for the user
+        },
+    })
+            if (!group) {
+        return (
+            <div className="flex flex-col w-full space-y-4 font-bold justify-center">
+                <h2 className="justify-center">Create your first Group</h2>
+            </div>
+        );
+    }
+
+    return (
+        <div className="h-full w-full flex flex-col space-y-8 md:grid grid-cols-2 md:space-x-6">
+                {groups.map((group) => (
+                    <GroupBox
+                        key={profile.id}
+                        name={profile.name}
+                        imageUrl={profile.imageUrl}
+                        type={profile.type || null}
+                        email={profile.email || null}
+                    />
+                ))}
+            </div>
+    );
+};
 
 export default Orgs
