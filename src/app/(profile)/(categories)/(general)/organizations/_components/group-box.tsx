992b47826675aecc20e0f7b061152577a66d1838
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-15 20:17:51 +0100

    Create group-box.tsx

---
+++ src/app/(profile)/(categories)/(general)/organizations/_components/group-box.tsx
@@ -0,0 +1,71 @@
+"use client"
+
+
+import Link from "next/link"
+import GroupImage from "./group-image"
+
+
+
+type GBProps = {
+  href: string
+  image: string
+  name: string
+  description: string
+
+}
+
+
+const GroupBox = ({ image, href, name, description } : GBProps) => {
+return ( 
+  <Link href={href}>
+    <div
+      className="
+        w-full,
+        relative
+        flex
+        items-center
+        space-x-3
+        hover:bg-neutral-100
+        rounded-lg
+        transition
+        cursor-pointer
+        p-3
+      "
+    >
+        <GroupImage imageUrl={image} />
+      <div className="min-w-0 flex-1">
+        <div className="focus:outline-none">
+          <div
+            className="
+              flex
+              justify-between
+              items-center
+              mb-1
+            "
+          >
+            <p
+              className="
+                text-md
+                font-medium
+                text-gray-900
+              "
+            >
+              {name}
+            </p>
+          </div>
+          <p
+            className="
+              truncate
+              text-sm
+            "
+          >
+            {description}
+          </p>
+        </div>
+      </div>
+    </div>
+    </Link>
+   )
+}
+ 
+export default GroupBox;
