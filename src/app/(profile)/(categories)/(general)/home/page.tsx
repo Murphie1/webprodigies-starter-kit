c56c6aaf18c2d52a7f60a2f8badb68b1099485a2
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-16 19:26:22 +0100

    Update page.tsx

--- src/app/(profile)/(categories)/(general)/home/page.tsx
+++ src/app/(profile)/(categories)/(general)/home/page.tsx
@@ -4,12 +4,12 @@ const Homepage = () => {
     return (
         <div className="flex flex-col pt-6 space-y-2 items-center justify-center md:grid md:grid-cols-2 md:space-y-0 md:space-x-8">
             {/* Card 1 */}
-            <div className="flex items-center justify-center bg-gray-100 dark:bg-gray-800 border-1 border-black dark:border-themeBlack rounded-2xl w-[350px] h-[175px] hover:scale-105 transition-transform duration-300">
+            <div className="flex items-center justify-center bg-gray-100 dark:bg-gray-800 border-1 border-black dark:border-themeBlack rounded-2xl w-[350px] h-[175px] shadow-sm hover:scale-105 transition-transform duration-300">
                 <p className="text-2xl text-center">Text</p>
             </div>
 
             {/* Card 2 */}
-            <div className="hidden md:flex items-center justify-center bg-white dark:bg-gray-800 border-2 border-gray-300 dark:border-gray-600 rounded-2xl w-[500px] h-[300px] hover:scale-105 transition-transform duration-300">
+            <div className="hidden md:flex items-center justify-center bg-gray-100 dark:bg-gray-800 border-1 border-black dark:border-themeBlack rounded-2xl w-[350px] h-[175px] shadow-sm hover:scale-105 transition-transform duration-300">
                 <p className="text-2xl text-center">Text</p>
             </div>
             <Grids />
