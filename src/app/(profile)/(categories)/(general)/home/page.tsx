93a03f8c0830d51c59ef81849c1dd6461e4032b1
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-16 18:13:50 +0100

    Update page.tsx

--- src/app/(profile)/(categories)/(general)/home/page.tsx
+++ src/app/(profile)/(categories)/(general)/home/page.tsx
@@ -2,11 +2,11 @@
 
 const Homepage = () => {
     return (
-        <div className="flex md:grid grid-cols-2 mx-auto">
-        <div className="justify-center bg-themeWhite dark:bg-themeBlack border-2 rounded-md w-[500px] h-[300px]">
-           <p className="text-2xl">Text</p> 
+        <div className="flex pt-8 md:grid grid-cols-2 mx-auto">
+        <div className="justify-center bg-sky-300 dark:bg-themeGray border-2 rounded-lg w-[300px] h-[150px] hover:scale-310 transition-all">
+           <p className="text-2xl justify-center">Text</p> 
         </div>
-            <div className="hidden md:bg-themeWhite dark:bg-themeBlack border-2 rounded-md w-[500px] h-[300px]">
+            <div className="hidden md:bg-themeWhite dark:bg-themeBlack border-2 rounded-lg w-[500px] h-[300px]">
            <p className="text-2xl">Text</p> 
         </div>
             </div>
