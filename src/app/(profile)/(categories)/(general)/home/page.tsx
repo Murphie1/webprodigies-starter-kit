fe3f38009ba454b54d40af08c2a4603c11063e7c
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-16 18:08:00 +0100

    Update page.tsx

--- src/app/(profile)/(categories)/(general)/home/page.tsx
+++ src/app/(profile)/(categories)/(general)/home/page.tsx
@@ -1,7 +1,16 @@
 // pages/homepage.js
 
 const Homepage = () => {
-    return <div>Homepage</div>
+    return (
+        <div className="flex md:grid grid-cols-2 mx-auto">
+        <div className="justify-center bg-themeWhite dark:bg-themeBlack border-2 rounded-md w-[500px] h-[300px]">
+           <p className="text-2xl">Text</p> 
+        </div>
+            <div className="hidden md:bg-themeWhite dark:bg-themeBlack border-2 rounded-md w-[500px] h-[300px]">
+           <p className="text-2xl">Text</p> 
+        </div>
+            </div>
+        )
 }
 
 export default Homepage
