22a21a5541f72781ef2bc61f567afa1cfaec8517
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-16 17:31:16 +0100

    Update index.tsx

--- src/components/forms/media-gallery/index.tsx
+++ src/components/forms/media-gallery/index.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { useMediaGallery } from "@/hooks/groups"
-import { BadgePlus } from "@/icons"
+import { Badgeplus } from "@/icons"
 import { ErrorMessage } from "@hookform/error-message"
 
 type Props = {
