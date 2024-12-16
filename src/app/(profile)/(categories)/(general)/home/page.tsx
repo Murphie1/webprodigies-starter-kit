5da308af9bb9a742844dbceaa7de5f2441c72e2c
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-16 19:15:22 +0100

    Update page.tsx

--- src/app/(profile)/(categories)/(general)/home/page.tsx
+++ src/app/(profile)/(categories)/(general)/home/page.tsx
@@ -4,7 +4,7 @@ const Homepage = () => {
     return (
         <div className="flex flex-col pt-6 space-y-2 items-center justify-center md:grid md:grid-cols-2 md:space-y-0 md:space-x-8">
             {/* Card 1 */}
-            <div className="flex items-center justify-center bg-gray-50 dark:bg-themeGray border-2 border-black dark:border-themeBlack rounded-2xl w-[350px] h-[175px] hover:scale-105 transition-transform duration-300">
+            <div className="flex items-center justify-center bg-gray-100 dark:bg-gray-800 border-1 border-black dark:border-themeBlack rounded-2xl w-[350px] h-[175px] hover:scale-105 transition-transform duration-300">
                 <p className="text-2xl text-center">Text</p>
             </div>
 
