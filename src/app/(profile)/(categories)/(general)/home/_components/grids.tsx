0ff0f3ae97e7ea1483d10827047bf2ed39709378
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-16 18:58:36 +0100

    Update grids.tsx

--- src/app/(profile)/(categories)/(general)/home/_components/grids.tsx
+++ src/app/(profile)/(categories)/(general)/home/_components/grids.tsx
@@ -7,16 +7,16 @@ const Grids = () => {
     ];
 
     return (
-        <div className="flex flex-wrap items-center justify-center gap-4 md:gap-6 p-4">
+        <div className="flex flex-wrap items-center justify-center grid grid-cols-2 md:grid-cols-4 md:gap-6 p-4">
             {categories.map((category, index) => (
                 <div
                     key={index}
                     className="flex items-center justify-between w-[170px] h-[80px] bg-gray-100 dark:bg-gray-800 rounded-2xl shadow-md transition-transform duration-300 hover:scale-105"
                 >
-                    <span className="text-lg text-blue-500 mb-2">{category.icon}</span>
-                    <p className="text-lg font-medium text-gray-800 dark:text-white">
+                    <p className="text-lg pl-1 font-medium text-gray-800 dark:text-white">
                         {category.name}
                     </p>
+                    <span className="text-lg text-blue-500 pr-1">{category.icon}</span>
                 </div>
             ))}
         </div>
