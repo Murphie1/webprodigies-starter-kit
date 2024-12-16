35ea291966597b33429baa20c6e72dde15f814d7
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-16 18:43:36 +0100

    Update page.tsx

--- src/app/(profile)/(categories)/(general)/home/page.tsx
+++ src/app/(profile)/(categories)/(general)/home/page.tsx
@@ -1,6 +1,8 @@
+import { Grids } from "./_components/grids"
+
 const Homepage = () => {
     return (
-        <div className="flex flex-col pt-6 items-center justify-center md:grid md:grid-cols-2 md:space-y-0 md:space-x-8">
+        <div className="flex flex-col pt-6 space-y-6 items-center justify-center md:grid md:grid-cols-2 md:space-y-0 md:space-x-8">
             {/* Card 1 */}
             <div className="flex items-center justify-center bg-gray-50 dark:bg-themeGray border-2 border-black dark:border-themeBlack rounded-2xl w-[350px] h-[175px] hover:scale-105 transition-transform duration-300">
                 <p className="text-2xl text-center">Text</p>
@@ -10,6 +12,7 @@ const Homepage = () => {
             <div className="hidden md:flex items-center justify-center bg-white dark:bg-gray-800 border-2 border-gray-300 dark:border-gray-600 rounded-2xl w-[500px] h-[300px] hover:scale-105 transition-transform duration-300">
                 <p className="text-2xl text-center">Text</p>
             </div>
+            <Grids />
         </div>
     );
 };
