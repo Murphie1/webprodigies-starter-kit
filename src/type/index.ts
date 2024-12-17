6e245fa8c5eaae7fdd709e5add20c1c1dece3444
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-17 21:00:57 +0100

    Update index.ts

--- src/type/index.ts
+++ src/type/index.ts
@@ -3,7 +3,17 @@
 import { Dispatch, SetStateAction } from "react"
 
 import { Id } from "~/convex/_generated/dataModel"
+import { Conversation, Message, User } from "@prisma/client";
 
+export type FullMessageType = Message & {
+  sender: User,
+  seen: User[]
+};
+
+export type FullConversationType = Conversation & {
+  users: User[],
+  messages: FullMessageType[],
+};
 export interface EmptyStateProps {
     title: string
     search?: boolean
