8f61f3b4e6ddd8db9a6e9e261457f51debea6f5b
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-17 18:51:58 +0100

    Create auth.ts

---
+++ src/app/api/pusher/auth.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { currentUser } from "@clerk/nextjs/server"
+
+import { pusherServer } from "@/lib/pusher"
+
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  const session = await currentUser()
+
+  if (!session.emailAddresses[0]?.emailAddress) {
+    return response.status(401)
+  }
+
+  const socketId = request.body.socket_id;
+  const channel = request.body.channel_name;
+  const data = {
+    user_id: session.emailAddresses[0]?.emailAddress
+  }
+
+  const authResponse = pusherServer.authorizeChannel(socketId, channel, data)
+
+  return response.send(authResponse)
+}
