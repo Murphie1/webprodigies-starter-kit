f915dffc015e6636cc036a3a8ab0c94b0c2045b6
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-17 22:26:12 +0100

    Create MobileHeader.tsx

---
+++ src/components/uMessage/sidebar/MobileHeader.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import useConversation from "@/app/hooks/useConversation";
+import useRoutes from "@/hooks/uMessage/useRoutes";
+
+import MobileItem from "./MobileItem";
+
+const MobileHeader = () => {
+  const routes = useRoutes();
+  const { isOpen } = useConversation();
+
+  if (isOpen) {
+    return null;
+  }
+
+  return ( 
+    <div
+      className="
+        fixed
+        justify-between
+        overflow-x-auto
+        whitespace-nowrap
+        w-screen
+        top-0
+        z-40
+        flex
+        items-center
+        bg-white
+        dark:bg-black
+        border-b-[1px]
+        lg:hidden
+      "
+    >
+      {routes.map((route) => (
+        <MobileItem
+          key={route.href}
+          href={route.href}
+          active={route.active}
+          icon={route.icon}
+        />
+      ))}
+    </div>
+   );
+}
+ 
+export default MobileHeader;
