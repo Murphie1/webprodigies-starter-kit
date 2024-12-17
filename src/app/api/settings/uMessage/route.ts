8a3d4af1ec7f1b5c196b6def77c4582597e67240
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-17 18:58:35 +0100

    Create route.ts

---
+++ src/app/api/settings/uMessage/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+
+import { currentUser } from "@clerk/nextjs/server";
+import { client } from "@/lib/prisma";
+
+export async function POST(
+  request: Request
+) {
+  try {
+    const clerk = await currentUser();
+    const body = await request.json();
+    const {
+      name,
+      image
+    } = body;
+
+    if (!clerk.id) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    const updatedUser = await client.user.update({
+      where: {
+        id: currentUser.id
+      },
+      data: {
+        image: image,
+        firstname: name
+      }
+    });
+
+    return NextResponse.json(updatedUser)
+  } catch (error: any) {
+    console.log(error, 'ERROR_SETTINGS');
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+      }
