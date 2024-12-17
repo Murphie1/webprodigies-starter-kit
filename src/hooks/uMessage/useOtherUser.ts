1a08c123e12cb6692925bd25d5c08f79a6f9b062
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-17 21:01:35 +0100

    Create useOtherUser.ts

---
+++ src/hooks/uMessage/useOtherUser.ts
@@ -0,0 +1,26 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { useMemo } from "react";
+import { FullConversationType } from "@/type";
+import { User } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const useOtherUser = async (conversation: FullConversationType | {
+  users: User[]
+}) => {
+  const clerk = await currentUser();
+  if (!clerk || clerk.emailAddresses[0]?.emailAddress) {
+      return new NextResponse('Unauthorized', { status: 401 });
+  }
+
+  const otherUser = useMemo(() => {
+    const currentUserEmail = clerk.emailAddresses[0]?.emailAddress;
+
+    const otherUser = conversation.users.filter((user) => user.email !== currentUserEmail);
+
+    return otherUser[0];
+  }, [clerk.emailAddresses[0]?.emailAddress, conversation.users]);
+
+  return otherUser;
+};
+
+export default useOtherUser;
