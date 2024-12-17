cf8c4c959f8f8813521aa84e439db1ba590acfb9
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-17 23:10:26 +0100

    Create MobileItem.tsx

---
+++ src/components/uMessage/sidebar/MobileItem.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+
+interface MobileItemProps {
+  href: string;
+  icon: any;
+  active?: boolean;
+  onClick?: () => void;
+}
+
+const MobileItem: React.FC<MobileItemProps> = ({
+  href,
+  icon: Icon,
+  active,
+}) => {
+  
+
+  return ( 
+    <Link 
+      href={href}
+      className={cn(`
+        group
+        flex
+        gap-x-3
+        text-sm
+        leading-6
+        font-semibold
+        w-full
+        justify-center
+        p-4
+        text-gray-500
+        dark:text-themeTextWhite
+        hover:text-black
+        dark:hover:text-white
+        hover:bg-gray-100
+        dark:hover:bg-sky-700
+      `,
+        active && "bg-gray-100 text-black dark:bg-sky-700 dark:text-white"
+      )}
+    >
+      <Icon className="h-10 w-10" />
+    </Link>
+   );
+}
+ 
+export default MobileItem;
