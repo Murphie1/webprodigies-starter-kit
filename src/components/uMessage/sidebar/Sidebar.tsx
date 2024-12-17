8ef3ec9ddfc42ff2a922e783853d6dd43b3f6323
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-17 22:29:17 +0100

    Update Sidebar.tsx

--- src/components/uMessage/sidebar/Sidebar.tsx
+++ src/components/uMessage/sidebar/Sidebar.tsx
@@ -12,7 +12,7 @@ async function Sidebar({ children }: {
     <div className="h-full">
       <DesktopSidebar currentUser={currentUser!} />
       <MobileHeader />
-      <main className="h-[700px] w-[calc(100vw-10px)] sm:overflow-y-auto justify-center md:w-full lg:pl-20 lg:h-full">
+      <main className="h-[700px] w-[calc(100vw-10px)] bg-themeWhite dark:bg-gray-900 sm:rounded-2xl border-1 border-gray-500 dark:border-themeGray sm:overflow-y-auto justify-center md:w-full lg:pl-20 lg:h-full">
         {children}
       </main>
     </div>
