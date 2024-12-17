559c5e5699de8644c7bd12c6a7944012d2b175d1
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-17 19:11:06 +0100

    Create route.ts

---
+++ src/app/api/uMessage/route.ts
@@ -0,0 +1,90 @@
+import { onAuthenticatedUser }  from "@/actions/auth";
+import { currentUser } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import { client } from "@/lib/prisma";
+import { pusherServer } from "@/lib/pusher";
+
+export async function POST(
+  request: Request
+) {
+  try {
+    const clerk = await currentUser();
+    const localUser = await onAuthenticatedUser();
+    
+    
+    const body = await request.json();
+    const {
+      message,
+      image,
+      conversationId
+    } = body;
+
+    if (!localUser?.id || !clerk.emailAddresses[0]?.emailAddress) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    const newMessage = await client.chat.create({
+      data: {
+        body: message,
+        image: image,
+        conversation: {
+          connect: {
+            id: conversationId
+          }
+        },
+        sender: {
+          connect: {
+            id: localUser.id
+          }
+        },
+        seen: {
+          connect: {
+            id: localUser.id
+          }
+        }
+      },
+      include: {
+        seen: true,
+        sender: true,
+      }
+    });
+
+    const updatedConversation = await client.conversation.update({
+      where: {
+        id: conversationId
+      },
+      data: {
+        lastMessageAt: new Date(),
+        chats: {
+          connect: {
+            id: newMessage.id
+          }
+        }
+      },
+      include: {
+        users: true,
+        chats: {
+          include: {
+            seen: true
+          }
+        }
+      }
+    });
+
+    await pusherServer.trigger(conversationId, 'messages:new', newMessage);
+
+    const lastMessage = updatedConversation.chats[updatedConversation.chats.length - 1];
+
+    updatedConversation.users.map((user) => {
+      pusherServer.trigger(user.email!, 'conversation:update', {
+        id: conversationId,
+        chats: [lastMessage]
+      })
+    });
+
+    return NextResponse.json(newMessage);
+  } catch (error: any) {
+    console.log(error, 'ERROR_MESSAGES');
+    return new NextResponse('InternalError', { status: 500 });
+  }
+      }
