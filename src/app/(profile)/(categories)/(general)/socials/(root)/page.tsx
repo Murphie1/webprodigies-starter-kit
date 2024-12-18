9d3c2d7e7684fca9236229ab8c2ac963890daa81
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-18 08:05:35 +0100

    Create page.tsx

---
+++ src/app/(profile)/(categories)/(general)/socials/(root)/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+
+import useConversation from "@/hooks/uMessage/useConversation";
+import EmptyStateTwo from "@/components/uMessage/EmptyState";
+
+const Home = () => {
+  const { isOpen } = useConversation();
+
+  return (
+    <div
+      className={cn(
+        "lg:pl-80 h-full lg:block",
+        isOpen ? 'block' : 'hidden'
+      )}
+    >
+      <EmptyStateTwo />
+    </div>
+  )
+};
+
+export default Home;
