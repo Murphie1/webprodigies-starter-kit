77a6aaa3aebf890d6a705cf73f1857db974cf80d
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-18 13:14:34 +0100

    Create page.tsx

---
+++ src/app/(profile)/(categories)/(general)/socials/friends/page.tsx
@@ -0,0 +1,11 @@
+import EmptyStateTwo from "@/components/uMessage/EmptyState";
+
+const Friends = () => {
+  return ( 
+    <div className="hidden lg:block lg:pl-80 h-full">
+      <EmptyStateTwo />
+    </div>
+   );
+}
+ 
+export default Friends;
