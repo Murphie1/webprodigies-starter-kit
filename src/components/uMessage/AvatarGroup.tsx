8c90cf14e03bc1c5b6a454f752c12c637ffe0d1a
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-19 09:00:34 +0100

    Update AvatarGroup.tsxp

--- src/components/uMessage/AvatarGroup.tsx
+++ src/components/uMessage/AvatarGroup.tsx
@@ -4,25 +4,26 @@ import { User, Conversation } from "@prisma/client";
 import Image from "next/image";
 
 interface AvatarGroupProps {
-  users?: User[],
-  conversation?: Conversation
+  users?: User[];
+  conversation?: Conversation;
 }
 
 const AvatarGroup: React.FC<AvatarGroupProps> = ({
   conversation,
-  users = []
+  users = [],
 }) => {
   const slicedUsers = users.slice(0, 3);
 
   const positionMap = {
-    0: 'top-0 left-[12px]',
-    1: 'bottom-0',
-    2: 'bottom-0 right-0'
+    0: "top-0 left-[12px]",
+    1: "bottom-0",
+    2: "bottom-0 right-0",
   };
 
-  return ( 
-    {conversation.imageUrl ? (
-      <div
+  return (
+    <div>
+      {conversation?.imageUrl ? (
+        <div
           className="
             absolute
             inline-block
@@ -32,43 +33,40 @@ const AvatarGroup: React.FC<AvatarGroupProps> = ({
             w-[21px]
           "
         >
-          <Image
-            alt="Avatar"
-            fill
-            src={conversation.imageUrl!}
-          />
+          <Image alt="Avatar" fill src={conversation.imageUrl} />
         </div>
       ) : (
-    <div
-      className="
-        relative
-        h-11
-        w-11
-      "
-    >
-      {slicedUsers.map((user, index) => (
         <div
-          key={user.id}
-          className={`
-            absolute
-            inline-block
-            rounded-full
-            overflow-hidden
-            h-[21px]
-            w-[21px]
-            ${positionMap[index as keyof typeof positionMap]}
-          `}
+          className="
+            relative
+            h-11
+            w-11
+          "
         >
-          <Image
-            alt="Avatar"
-            fill
-            src={user?.image || '/next.svg'}
-          />
+          {slicedUsers.map((user, index) => (
+            <div
+              key={user.id}
+              className={`
+                absolute
+                inline-block
+                rounded-full
+                overflow-hidden
+                h-[21px]
+                w-[21px]
+                ${positionMap[index as keyof typeof positionMap]}
+              `}
+            >
+              <Image
+                alt="Avatar"
+                fill
+                src={user?.image || "/next.svg"}
+              />
+            </div>
+          ))}
         </div>
-      ))}
+      )}
     </div>
-        )};
-   );
-}
- 
+  );
+};
+
 export default AvatarGroup;
