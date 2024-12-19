fd6a42bd9a7ef1e03894c6871b7c6ca558efaf9a
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-19 17:46:43 +0100

    Update useOtherFriendRequest.ts

--- src/hooks/uMessage/useOtherFriendRequest.ts
+++ src/hooks/uMessage/useOtherFriendRequest.ts
@@ -1,4 +1,4 @@
-import { loggedInUser } from "@clerk/nextjs/server"
+import { loggedInUser } from "@/actions/auth"
 import { useMemo } from "react"
 import { FullFriendRequestType } from "@/type"
 import { User } from "@prisma/client"
