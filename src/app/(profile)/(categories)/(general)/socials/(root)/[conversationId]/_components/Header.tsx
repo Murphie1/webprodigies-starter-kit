17e4b18dc172a55fb9dd556749a821cfd9b52066
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-20 10:00:58 +0100

    Update Header.tsx

--- src/app/(profile)/(categories)/(general)/socials/(root)/[conversationId]/_components/Header.tsx
+++ src/app/(profile)/(categories)/(general)/socials/(root)/[conversationId]/_components/Header.tsx
@@ -1,14 +1,9 @@
-
-
 import { Conversation, User } from "@prisma/client"
-
-import useOtherUser from "@/hooks/uMessage/useOtherUser"
 import { useMemo } from "react"
 import Link from "next/link"
 import { ChevronLeft } from "lucide-react"
 import Avatar from "@/components/uMessage/Avatar"
 import AvatarGroup from "@/components/uMessage/AvatarGroup"
-
 import ProfileDrawer from "./ProfileDrawer"
 import useActiveList from "@/hooks/uMessage/useActiveList"
 
@@ -16,11 +11,10 @@ interface HeaderProps {
     conversation: Conversation & {
         users: User[]
     }
+    otherUser: User | null
 }
 
-const Header: React.FC<HeaderProps> = async ({ conversation }) => {
-    const otherUser = await useOtherUser(conversation)
-
+const Header: React.FC<HeaderProps> = ({ conversation, otherUser }) => {
     const { members } = useActiveList()
     const isActive = members.indexOf(otherUser?.email!) !== -1
 
@@ -33,62 +27,60 @@ const Header: React.FC<HeaderProps> = async ({ conversation }) => {
     }, [conversation, isActive])
 
     return (
-        <>
-            <div
-                className="
-          bg-themeWhite
-          dark:bg-themeBlack
-          w-full
-          flex
-          border-b-[1px]
-          sm:px-4
-          py-3
-          px-4
-          lg:px-6
-          justify-between
-          items-center
-          shadow-sm
-        "
-            >
-                <div className="flex gap-3 items-center">
-                    <Link
-                        className="
-              lg:hidden
-              block
-              text-sky-500
-              dark:text-themeGray
-              hover:text-sky-600
-              transition
-              cursor-pointer
+        <div
+            className="
+                bg-themeWhite
+                dark:bg-themeBlack
+                w-full
+                flex
+                border-b-[1px]
+                sm:px-4
+                py-3
+                px-4
+                lg:px-6
+                justify-between
+                items-center
+                shadow-sm
             "
-                        href="/socials"
+        >
+            <div className="flex gap-3 items-center">
+                <Link
+                    className="
+                        lg:hidden
+                        block
+                        text-sky-500
+                        dark:text-themeGray
+                        hover:text-sky-600
+                        transition
+                        cursor-pointer
+                    "
+                    href="/socials"
+                >
+                    <ChevronLeft size={22} />
+                </Link>
+                {conversation.isGroup ? (
+                    <AvatarGroup
+                        conversation={conversation}
+                        users={conversation.users}
+                    />
+                ) : (
+                    <Avatar user={otherUser} />
+                )}
+                <div className="flex flex-col">
+                    <div>{conversation.name || otherUser?.firstname}</div>
+                    <div
+                        className="
+                            text-sm
+                            font-light
+                            text-neutral-500
+                        "
                     >
-                        <ChevronLeft size={22} />
-                    </Link>
-                    {conversation.isGroup ? (
-                        <AvatarGroup
-                            conversation={conversation}
-                            users={conversation.users}
-                        />
-                    ) : (
-                        <Avatar user={otherUser} />
-                    )}
-                    <div className="flex flex-col">
-                        <div>{conversation.name || otherUser.firstname}</div>
-                        <div
-                            className="
-                text-sm
-                font-light
-                text-neutral-500
-              "
-                        >
-                            {statusText}
-                        </div>
+                        {statusText}
                     </div>
                 </div>
-                <ProfileDrawer data={conversation} />
             </div>
-        </>
+            <ProfileDrawer data={conversation} />
+        </div>
     )
 }
 
