370976fec3d8599e0e0709e7960c2c5d9ac54aeb
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-20 10:46:43 +0100

    Update ImageModal.tsx

--- src/app/(profile)/(categories)/(general)/socials/(root)/[conversationId]/_components/ImageModal.tsx
+++ src/app/(profile)/(categories)/(general)/socials/(root)/[conversationId]/_components/ImageModal.tsx
@@ -2,7 +2,7 @@
 
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import Image from "next/image";
-
+import { FullMessageType } from "@/type";
 interface ImageModalProps {
   src?: FullMessageType | null;
 }
