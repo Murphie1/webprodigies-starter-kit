7c05bdcd653aa0e825d1bf4915748293788084ca
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-21 10:58:37 +0100

    Update Sidebar.tsx

--- src/components/uMessage/sidebar/Sidebar.tsx
+++ src/components/uMessage/sidebar/Sidebar.tsx
@@ -11,7 +11,7 @@ async function Sidebar({ children }: { children: React.ReactNode }) {
         <div className="h-full">
             <DesktopSidebar loggedUser={user!} />
             <MobileHeader />
-            <main className="h-[700px] w-[calc(100vw-10px)] bg-themeWhite dark:bg-gray-900 sm:rounded-2xl border-1 border-gray-500 dark:border-themeGray sm:overflow-y-auto justify-center md:w-full lg:pl-20 lg:h-full">
+            <main className="h-[500px] w-[calc(100vw-10px)] pl-[5px] sm:pt-[70px] sm:rounded-2xl border-1 border-gray-500 dark:border-themeGray sm:overflow-y-auto md:w-full lg:pl-20 lg:h-full">
                 {children}
             </main>
         </div>
