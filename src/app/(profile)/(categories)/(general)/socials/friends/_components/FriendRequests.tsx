99bd20e323beb96a9362421ac12c0b31e1670669
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-21 11:48:02 +0100

    Update FriendRequests.tsx

--- src/app/(profile)/(categories)/(general)/socials/friends/_components/FriendRequests.tsx
+++ src/app/(profile)/(categories)/(general)/socials/friends/_components/FriendRequests.tsx
@@ -18,7 +18,7 @@ export const FriendRequests = async () => {
 
     return (
         <Drawer>
-            <DrawerTrigger className="px-4 py-2 text-sm font-medium transition bg-blue-500 rounded-md text-white hover:bg-blue-600 dark:bg-blue-700 dark:hover:bg-blue-800">
+            <DrawerTrigger className="px-4 py-2 w-40 md:w-56 justify-center text-sm font-medium transition bg-blue-500 rounded-md text-white hover:bg-blue-600 dark:bg-blue-700 dark:hover:bg-blue-800">
                 Friend Requests
             </DrawerTrigger>
             <DrawerContent className="h-auto max-h-[calc(100vh-50px)] overflow-y-auto bg-white dark:bg-gray-900 text-black dark:text-gray-100 shadow-lg">
