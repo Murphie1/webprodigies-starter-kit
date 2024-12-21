70cfb148cea070dede83166fd3187e492ff2dec1
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-21 18:54:00 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,10 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: "https",
+                hostname: "utfs.io",
+            },
+            {
+                protocol: "https",
                 hostname: "ucarecdn.com",
             },
             {
