10e80ce7a7699c47152863f420dcf5dc1cf4e49a
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-21 11:23:22 +0100

    Update layout.tsx

--- src/app/(profile)/(categories)/(general)/socials/friends/layout.tsx
+++ src/app/(profile)/(categories)/(general)/socials/friends/layout.tsx
@@ -1,7 +1,7 @@
 import { getFriends } from "@/actions/uMessage"
 import Sidebar from "@/components/uMessage/sidebar/Sidebar"
 import FriendList from "./_components/FriendList"
-import { FriendRequests } from "./_components/FriendRequests"
+
 
 export default async function FriendsLayout({
     children,
@@ -14,9 +14,6 @@ export default async function FriendsLayout({
         
         <Sidebar>
             <div className="h-full sm:rounded-2xl bg-themeWhite dark:bg-gray-900">
-                <a className="pt-8">
-                    <FriendRequests />
-                </a>
                 <FriendList items={friends} />
                 {children}
             </div>
