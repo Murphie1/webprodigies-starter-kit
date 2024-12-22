c72d9429915e1393e69240ed2c74d0f04bc10ba1
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-22 08:01:22 +0100

    Update layout.tsx

--- src/app/(profile)/(categories)/(general)/socials/(root)/layout.tsx
+++ src/app/(profile)/(categories)/(general)/socials/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import { getConversations } from "@/actions/uMessage"
 import { getFriends } from "@/actions/uMessage"
-import Sidebar from "@/components/uMessage/sidebar/Sidebar"
+//import Sidebar from "@/components/uMessage/sidebar/Sidebar"
 import ConversationList from "./_components/ConversationList"
 
 export default async function ConversationsLayout({
@@ -12,11 +12,11 @@ export default async function ConversationsLayout({
     const users = await getFriends()
 
     return (
-        <Sidebar>
+       // <Sidebar>
             <div className="h-full bg-themeWhite rounded-2xl pt-8 dark:bg-gray-900">
                 <ConversationList users={users} initialItems={conversations} />
                 {children}
             </div>
-        </Sidebar>
+       // </Sidebar>
     )
 }
