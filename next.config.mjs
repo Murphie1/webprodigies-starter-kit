16e252c865df0d42a291df1c71933d93ae847b15
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-22 08:27:45 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,41 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "utfs.io",
-            },
-            {
-                protocol: "https",
-                hostname: "ucarecdn.com",
-            },
-            {
-                protocol: "https",
-                hostname: "img.clerk.com",
-            },
-            {
-                protocol: "https",
-                hostname: "lovely-flamingo-139.convex.cloud",
-            },
-            {
-                protocol: "https",
-                hostname: "combative-meerkat-957.convex.cloud",
-            },
-        ],
-        domains: ["uploadthing.com"],
-    },
-}
+  reactStrictMode: false,
+  images: {
+    // Combine remotePatterns and domains
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+      {
+        protocol: "https",
+        hostname: "ucarecdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lovely-flamingo-139.convex.cloud",
+      },
+      {
+        protocol: "https",
+        hostname: "combative-meerkat-957.convex.cloud",
+      },
+    ],
+    domains: [
+      "uploadthing.com",
+      "res.cloudinary.com",
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+    ],
+  },
+  // Add the SWC plugin for next-superjson-plugin
+  swcPlugins: [
+    ["next-superjson-plugin", {}],
+  ],
+};
 
-export default nextConfig
+export default nextConfig;
