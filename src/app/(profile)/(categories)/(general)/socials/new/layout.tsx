5ff066b8bafb57d597440c0038fbafc390aadacc
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-23 13:42:40 +0100

    Create layout.tsx

---
+++ src/app/(profile)/(categories)/(general)/socials/new/layout.tsx
@@ -0,0 +1,27 @@
+import LeftPanel from "@/components/uMessage/home/left-panel"
+import { usePathname } from "next/navigation"
+
+type Props = {
+    children: React.ReactNode
+}
+
+const SocialLayout = ({ children }: Props) => {
+
+        const pathname = usePathname()
+     return (
+      <div className="flex flex-1 pt-[50px]">
+			<div className='flex overflow-y-hidden h-[calc(100vh-50px)] max-w-[1700px] mx-auto bg-left-panel'>
+            <div className="hidden md:fixed top-0 left-0 w-full h-36 bg-green-primary dark:bg-transparent -z-30" />
+                <LeftPanel />
+            </div>
+  {pathname === "/socials/new" ? (
+     <div className="w-full h-full relative md:hidden">
+       <LeftPanel />
+       </div>)}
+            <div className="h-full md:pl-1/3">
+                {children}
+            </div>
+        </div>
+  
+    )
+}
