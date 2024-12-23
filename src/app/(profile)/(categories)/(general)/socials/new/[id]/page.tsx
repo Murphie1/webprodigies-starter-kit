8b0a55499372f649bb8ba35d0cdc9c3a8a126988
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-23 13:49:30 +0100

    Create page.tsx

---
+++ src/app/(profile)/(categories)/(general)/socials/new/[id]/page.tsx
@@ -0,0 +1,12 @@
+
+import RightPanel from "@/components/uMessage/home/right-panel";
+import { useTheme } from "next-themes";
+
+export defaultyoub function Pages() {
+	return (
+		<main className='m-5'>
+				<RightPanel />
+		</main>
+	);
+  }
+export default Pages;
