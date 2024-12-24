65ab9b0e9a3e50932d8544aa53477fef9d483bc2
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-24 09:13:49 +0100

    Update page.tsx

--- src/app/(profile)/(categories)/(general)/socials/new/[id]/page.tsx
+++ src/app/(profile)/(categories)/(general)/socials/new/[id]/page.tsx
@@ -2,11 +2,11 @@
 import RightPanel from "@/components/uMessage/home/right-panel";
 import { useTheme } from "next-themes";
 
-export default function Pages() {
+const Pages = () => {
 	return (
 		<main className='relative flex h-full w-full'>
 				<RightPanel />
 		</main>
 	);
   }
-
+export default Pages;
