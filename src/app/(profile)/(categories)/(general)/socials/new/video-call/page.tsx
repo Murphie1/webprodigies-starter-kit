23734b06e46880c53fea9a8d288ae0c2f2696d46
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-24 09:07:02 +0100

    Update page.tsx

--- src/app/(profile)/(categories)/(general)/socials/new/video-call/page.tsx
+++ src/app/(profile)/(categories)/(general)/socials/new/video-call/page.tsx
@@ -1,8 +1,9 @@
-"use client";
-import dynamic from "next/dynamic";
+//"use client";
+//import dynamic from "next/dynamic";
 
 //const DynamicVideoUI = dynamic(() => import("./video-ui-kit"), { ssr: false });
 
 export default function VideoCall() {
-	return <div>Video Calling </div>//<DynamicVideoUI />;
+	return <div>Video Calling </div>;
+	//<DynamicVideoUI />;
 }
