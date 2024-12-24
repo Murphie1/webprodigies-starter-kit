903c0495fb4c0023a88f8b0fca867b6291ba983c
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-24 10:14:26 +0100

    Update page.tsx

--- src/app/(profile)/(categories)/(general)/socials/new/page.tsx
+++ src/app/(profile)/(categories)/(general)/socials/new/page.tsx
@@ -1,35 +1,11 @@
-//import Grids from "./_components/grids"
-
-
-const Homepage = () => {
-    return (
-        <div className="flex flex-col pt-6 space-y-2 items-center justify-center md:grid md:grid-cols-2 md:space-y-0 md:space-x-8">
-            {/* Card 1 */}
-            <div className="flex items-center justify-center bg-gray-100 dark:bg-gray-800 border-1 border-black dark:border-themeBlack rounded-2xl w-[350px] h-[175px] shadow-sm hover:scale-105 transition-transform duration-300">
-                <p className="text-2xl text-center">Text</p>
-            </div>
-
-            {/* Card 2 */}
-            <div className="hidden md:flex items-center justify-center bg-gray-100 dark:bg-gray-800 border-1 border-black dark:border-themeBlack rounded-2xl w-[350px] h-[175px] shadow-sm hover:scale-105 transition-transform duration-300">
-                <p className="text-2xl text-center">Text</p>
-            </div>
-		{/* <Grids />*/}
-        </div>
-    )
-}
-
-export default Homepage
-
-{/*//"use client"
-//import ChatPlaceHolder from "@/components/uMessage/home/chat-placeholder";
+import ChatPlaceHolder from "@/components/uMessage/home/chat-placeholder";
 
 
 const SocialsHome = () => {
 	return (
-		<p> No chats </p>
-		//<main className='hidden md:flex relative'>
-	//<ChatPlaceHolder />
-		//</main>
+		<main className='hidden md:flex relative'>
+	<ChatPlaceHolder />
+		</main>
 	);
         }
-export default SocialsHome;*/}
+export default SocialsHome;
