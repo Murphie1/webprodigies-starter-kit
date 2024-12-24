44d4527fc07355e182168115e30518c86f86480e
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-24 08:13:40 +0100

    Update layout.tsx

--- src/app/(profile)/(categories)/(general)/socials/new/layout.tsx
+++ src/app/(profile)/(categories)/(general)/socials/new/layout.tsx
@@ -8,13 +8,16 @@ type Props = {
 const SocialLayout = ({ children }: Props) => {
 
         const pathname = usePathname()
+	const isHome = pathname === "/socials/new"
+
+	
      return (
       <div className="flex flex-1 pt-[50px]">
 			<div className='flex overflow-y-hidden h-[calc(100vh-50px)] max-w-[1700px] mx-auto bg-left-panel'>
             <div className="hidden md:fixed top-0 left-0 w-full h-36 bg-green-primary dark:bg-transparent -z-30" />
                 <LeftPanel />
             </div>
-  {pathname === "/socials/new" ? (
+  {isHome && (
      <div className="w-full h-full relative md:hidden">
        <LeftPanel />
        </div>)}
