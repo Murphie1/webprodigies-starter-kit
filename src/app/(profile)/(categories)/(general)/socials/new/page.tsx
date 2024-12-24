d79d3100dfcfef0884d571b4d7539b5195e9d076
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-24 09:12:35 +0100

    Update page.tsx

--- src/app/(profile)/(categories)/(general)/socials/new/page.tsx
+++ src/app/(profile)/(categories)/(general)/socials/new/page.tsx
@@ -1,7 +1,7 @@
 import ChatPlaceHolder from "@/components/uMessage/home/chat-placeholder";
 
 
-export default function Home() {
+export default function Home = () => {
 	return (
 		<main className='hidden md:flex relative'>
 	<ChatPlaceHolder />
