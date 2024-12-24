471787bc592a1182c57e609ace2885d6ee24cd51
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-24 08:28:54 +0100

    Create navigation.tsx

---
+++ src/app/(profile)/(categories)/(general)/socials/new/_components/navigation.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import LeftPanel from "@/components/uMessage/home/left-panel"
+import { usePathname } from "next/navigation"
+
+export const MobileLeftPanel = () => {
+
+  const pathname = usePathname()
+	const isHome = pathname === "/socials/new"
+  
+  
+
+
+return (
+  <section>
+  {isHome && (
+     <div className="w-full h-full relative md:hidden">
+       <LeftPanel />
+       </div>)}
+</section>
+  )
+}
