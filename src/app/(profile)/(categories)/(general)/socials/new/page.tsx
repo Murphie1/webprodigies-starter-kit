1182884ba8947e06f480f3d2a2247756bbb3ec63
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-24 09:44:07 +0100

    Update page.tsx

--- src/app/(profile)/(categories)/(general)/socials/new/page.tsx
+++ src/app/(profile)/(categories)/(general)/socials/new/page.tsx
@@ -1,12 +1,13 @@
-"use client"
-import ChatPlaceHolder from "@/components/uMessage/home/chat-placeholder";
+//"use client"
+//import ChatPlaceHolder from "@/components/uMessage/home/chat-placeholder";
 
 
 const SocialsHome = () => {
 	return (
-		<main className='hidden md:flex relative'>
-	<ChatPlaceHolder />
-		</main>
+		<p> No chats </p>
+		//<main className='hidden md:flex relative'>
+	//<ChatPlaceHolder />
+		//</main>
 	);
         }
 export default SocialsHome;
