c7cf53dde2de241b9ed09bb7231e8229f2f4a649
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2024-12-24 08:24:21 +0100

    Update layout.tsx

--- src/app/(profile)/(categories)/(general)/socials/new/layout.tsx
+++ src/app/(profile)/(categories)/(general)/socials/new/layout.tsx
@@ -1,5 +1,5 @@
 import LeftPanel from "@/components/uMessage/home/left-panel"
-import { usePathname } from "next/navigation"
+import { MobileLeftPanel } from "./_components/navigation"
 
 type Props = {
     children: React.ReactNode
@@ -7,20 +7,13 @@ type Props = {
 
 const SocialLayout = ({ children }: Props) => {
 
-        const pathname = usePathname()
-	const isHome = pathname === "/socials/new"
-
-	
      return (
       <div className="flex flex-1 pt-[50px]">
 			<div className='flex overflow-y-hidden h-[calc(100vh-50px)] max-w-[1700px] mx-auto bg-left-panel'>
             <div className="hidden md:fixed top-0 left-0 w-full h-36 bg-green-primary dark:bg-transparent -z-30" />
                 <LeftPanel />
             </div>
-  {isHome && (
-     <div className="w-full h-full relative md:hidden">
-       <LeftPanel />
-       </div>)}
+	      <MobileLeftPanel />
             <div className="h-full md:pl-1/3">
                 {children}
             </div>
