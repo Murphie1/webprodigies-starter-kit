753c9f7e74940f2bfbc47946bb5c465973d08193
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-01 09:05:02 +0100

    Update layout.tsx

--- src/app/(profile)/(categories)/(general)/socials/new/layout.tsx
+++ src/app/(profile)/(categories)/(general)/socials/new/layout.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const SocialLayout = ({ children }: Props) => {
     return (
-        <div className="flex flex-1 pt-[50px]">
+        <div className="flex flex-1">
             <div className="hidden md:flex overflow-y-hidden h-[calc(100vh-50px)] max-w-[1700px] mx-auto bg-left-panel">
                 <div className="hidden md:fixed top-0 left-0 w-full h-36 bg-green-primary dark:bg-transparent -z-30" />
                 <AsyncLeftPanel />
