8d0b55eef91d07a317b30839a4e24cd9b860d475
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-01 20:11:04 +0100

    Update user-list-dialog.tsx

--- src/components/uMessage/home/user-list-dialog.tsx
+++ src/components/uMessage/home/user-list-dialog.tsx
@@ -87,8 +87,6 @@ const users = friends?.map((friend) => friend.friendDetails) || [];
 					groupImage: storageId,
 				});
 			}
-
-			dialogCloseRef.current?.click();
 			setSelectedUsers([]);
 			setGroupName("");
 			setSelectedImage(null);
@@ -98,12 +96,13 @@ const users = friends?.map((friend) => friend.friendDetails) || [];
 
 			setSelectedConversation({
 				_id: conversationId,
-				participants: selectedUsers,
+				participants: [...selectedUsers, me?._id],
 				isGroup,
 				image: isGroup ? renderedImage : "https://images.unsplash.com/photo-1606787581180-b40a30072f4d",//users?.find((user) => user._id === selectedUsers[0])?.imageUrl! ?? "https://images.unsplash.com/photo-1606787581180-b40a30072f4d",
 				name: conversationName,
 				admin: me?._id!,
 			});
+			dialogCloseRef.current?.click();
 			setIsLoading(false);
 		} catch (err) {
 			toast.error("Failed to create conversation");
