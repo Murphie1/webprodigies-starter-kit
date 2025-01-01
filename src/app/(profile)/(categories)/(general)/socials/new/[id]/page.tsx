3d882bb4ffb309ac80ebbfb95ff6bca68fb240c7
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-01 22:04:00 +0100

    Update page.tsx

--- src/app/(profile)/(categories)/(general)/socials/new/[id]/page.tsx
+++ src/app/(profile)/(categories)/(general)/socials/new/[id]/page.tsx
@@ -1,10 +1,16 @@
 import RightPanel from "@/components/uMessage/home/right-panel"
 import { useTheme } from "next-themes"
+import { onAuthenticatedUser } from "@/actions/auth"
+import { redirect } from "next/navigation"
 
-const Pages = () => {
+
+const Pages = async () => {
+    const clerk = await onAuthenticatedUser()
+    if (!clerk || !clerk.clerkId) redirect("/")
+    
     return (
         <main className="relative flex h-full w-full">
-            <RightPanel />
+            <RightPanel clerkId={clerk.clerkId} />
         </main>
     )
 }
