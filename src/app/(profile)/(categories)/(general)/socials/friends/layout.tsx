8090ab266c06d7fb0bd15909717b17b2c8b3ba9d
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-01 13:54:12 +0100

    Update layout.tsx

--- src/app/(profile)/(categories)/(general)/socials/friends/layout.tsx
+++ src/app/(profile)/(categories)/(general)/socials/friends/layout.tsx
@@ -1,6 +1,3 @@
-import { getFriends } from "@/actions/uMessage"
-//import Sidebar from "@/components/uMessage/sidebar/Sidebar"
-import FriendList from "./_components/FriendList"
 import { FriendRequests } from "./_components/FriendRequests"
 
 export default async function FriendsLayout({
@@ -8,16 +5,13 @@ export default async function FriendsLayout({
 }: {
     children: React.ReactNode
 }) {
-    const friends = await getFriends()
 
     return (
-        //<Sidebar>
-        <div className="h-full rounded-2xl bg-themeWhite dark:bg-gray-900">
+        <div className="h-full">
             <div className="flex flex-col gap-y-3">
                 <FriendRequests />
             </div>
             {children}
-        </div>
-        // </Sidebar>
+        </div>    
     )
 }
