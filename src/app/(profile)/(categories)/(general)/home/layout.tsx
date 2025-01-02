ebf07fb49324b1b250877093dc8a6ccc2eb8d772
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-02 22:39:12 +0100

    Update layout.tsx

--- src/app/(profile)/(categories)/(general)/home/layout.tsx
+++ src/app/(profile)/(categories)/(general)/home/layout.tsx
@@ -9,17 +9,17 @@ const HomeLayout = ({ children }: Props) => {
     return (
         <div className="h-full">
             {/* Navbar */}
-            <div className="h-[50px] md:h-[75px] md:pl-56 fixed inset-y-0 w-full z-50">
+            <div className="h-[50px] md:h-[75px] md:pl-64 fixed inset-y-0 w-full z-50">
                 <NavBar />
             </div>
 
             {/* Sidebar */}
-            <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+            <div className="hidden md:flex h-full w-64 flex-col fixed inset-y-0 z-50">
                 <SideBar />
             </div>
 
             {/* Main Content */}
-            <main className="h-full md:pl-56 pt-[50px] md:pt-[75px]">
+            <main className="h-full md:pl-64 pt-[50px] md:pt-[75px]">
                 {children}
             </main>
         </div>
