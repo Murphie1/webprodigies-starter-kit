a1c01670e0609b349be9ebb84309c7faa346e8ba
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-02 22:30:15 +0100

    Create layout.tsx

---
+++ src/app/(profile)/(categories)/(general)/socials/conferencing/(home)/layout.tsx
@@ -0,0 +1,29 @@
+import { NavBar } from "@/components/navbar"
+import { SideBar } from "@/components/sidebar"
+
+type Props = {
+    children: React.ReactNode
+}
+
+const ConferenceLayout = ({ children }: Props) => {
+    return (
+        <div className="h-full">
+            {/* Navbar */}
+            <div className="h-[50px] md:h-[75px] md:pl-56 fixed inset-y-0 w-full z-50">
+                <NavBar />
+            </div>
+
+            {/* Sidebar */}
+            <div className="hidden md:flex h-full w-64 flex-col fixed inset-y-0 z-50">
+                <SideBar />
+            </div>
+
+            {/* Main Content */}
+            <main className="h-full md:pl-64 pt-[50px] md:pt-[75px]">
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default ConferenceLayout
