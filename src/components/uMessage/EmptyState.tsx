4e470edb6ddf9f7da5020e5d8c7ed5310f0ab896
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-02 10:37:57 +0100

    Update EmptyState.tsx

--- src/components/uMessage/EmptyState.tsx
+++ src/components/uMessage/EmptyState.tsx
@@ -1,42 +1,43 @@
-import Image from "next/image"
-import React from "react"
+import Image from "next/image";
+import React from "react";
 
 const EmptyStateTwo = () => {
-    return (
-        <div
-            className="
-        gap-y-4
-        h-full
-        w-screen
-        md:w-full
+  return (
+    <div
+      className="
         flex
-        flex-col-1
+        flex-col
         justify-center
         items-center
+        w-full
+        h-screen
+        overflow-y-auto
+        overflow-x-hidden
+        px-4
       "
-        >
-            <Image
-                src="/icons/emptyState.svg"
-                width={250}
-                height={250}
-                alt="empty state"
-            />
-
-            <div className="text-center items-center flex flex-col">
-                <h3
-                    className="
+    >
+      <Image
+        src="/icons/emptyState.svg"
+        width={250}
+        height={250}
+        alt="empty state"
+        className="mb-4"
+      />
+      <div className="text-center">
+        <h3
+          className="
             mt-2
             text-2xl
             font-semibold
             text-gray-900
             dark:text-gray-300
           "
-                >
-                    Select a chat or start a new conversation
-                </h3>
-            </div>
-        </div>
-    )
-}
+        >
+          Select a chat or start a new conversation
+        </h3>
+      </div>
+    </div>
+  );
+};
 
-export default EmptyStateTwo
+export default EmptyStateTwo;
