c9e2ebc9b0c6fdabd60786536534fad07f17548c
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-02 10:19:03 +0100

    Create page.tsx

---
+++ src/app/(profile)/(categories)/(general)/socials/(root)/page.tsx
@@ -0,0 +1,10 @@
+import EmptyStateTwo from "@/components/uMessage/EmptyState"
+
+const SocialsHome = () => {
+    return (
+        <main className="hidden md:flex relative">
+            <EmptyStateTwo />
+        </main>
+    )
+}
+export default SocialsHome
