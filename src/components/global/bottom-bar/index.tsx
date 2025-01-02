c3da77287d4474cb978a42bbdb1c5b0c67a2207b
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-02 13:25:51 +0100

    Update index.tsx

--- src/components/global/bottom-bar/index.tsx
+++ src/components/global/bottom-bar/index.tsx
@@ -8,7 +8,7 @@ import Link from "next/link"
 
 const BottomBar = () => {
     return (
-        <div className="bg-gray-100 space-x-1 h-[70px] w-[calc(100vw-20px)] shadow-lg fixed bottom-[10px] left-[10px] z-50 justify-between items-center flex dark:bg-themeBlack rounded-2xl">
+        <div className="bg-gray-100 space-x-1 h-[65px] w-[calc(100vw-20px)] shadow-lg fixed bottom-[10px] left-[10px] z-50 justify-between items-center flex dark:bg-themeBlack rounded-xl">
             <Link href={`/home`}>
                 <Telescope size={20} className="justify-center" />
                 <p className="text-bold text-sm">Home</p>
