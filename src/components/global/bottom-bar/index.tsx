d70b83bfa215f0a95396bc1629a61e771eef55a6
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-02 13:10:50 +0100

    Update index.tsx

--- src/components/global/bottom-bar/index.tsx
+++ src/components/global/bottom-bar/index.tsx
@@ -8,22 +8,22 @@ import Link from "next/link"
 
 const BottomBar = () => {
     return (
-        <div className="bg-white space-x-7 h-auto w-screen overflow-x-auto whitespace-nowrap py-3 px-11 fixed bottom-0 z-50 justify-between items-center flex dark:bg-[#1A1A1D]">
+        <div className="bg-white space-x-4 h-[70px] w-[calc(100vw-10px)] pl-[5px] overflow-hidden fixed bottom-2 z-50 justify-between items-center flex dark:bg-black">
             <Link href={`/home`}>
                 <Telescope />
-                <p className="text-bold text-black dark:text-white">Home</p>
+                <p className="text-bold text-md">Home</p>
             </Link>
             <Link href={`/updates`}>
                 <Lightbulb />
-                <p className="text-black text-bold dark:text-white">Updates</p>
+                <p className="text-md text-bold">Updates</p>
             </Link>
             <Link href={`/library`}>
                 <SquareLibrary />
-                <p className="text-black text-bold dark:text-white">Library</p>
+                <p className="text-md text-bold">Library</p>
             </Link>
             <Link href={`/socials`}>
                 <MessageCircle />
-                <p className="text-black text-bold dark:text-white">Socials</p>
+                <p className="text-md text-bold">Socials</p>
             </Link>
         </div>
     )
