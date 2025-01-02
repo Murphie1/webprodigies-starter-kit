863f520147c57e0eb463e11e5bd4d6306f682fdb
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-02 11:33:22 +0100

    Update layout.tsx

--- src/app/(profile)/(categories)/(general)/socials/(root)/layout.tsx
+++ src/app/(profile)/(categories)/(general)/socials/(root)/layout.tsx
@@ -1,18 +1,24 @@
-import AsyncLeftPanel from "@/components/uMessage/home/AsyncLeftPanel"
+import LeftPanel from "@/components/uMessage/home/left-panel"
 import { MobileLeftPanel } from "./_components/navigation"
+import { onAuthenticatedUser } from "@/actions/auth"
+import { redirect } from "next/navigation"
+
 
 type Props = {
     children: React.ReactNode
 }
 
-const SocialLayout = ({ children }: Props) => {
+const SocialLayout = async ({ children }: Props) => {
+    const clerk = await onAuthenticatedUser()
+    if (!clerk || !clerk.clerkId) redirect("/")
+    
     return (
         <div className="flex flex-1">
             <div className="hidden md:flex overflow-y-hidden h-[calc(100vh-50px)] max-w-[1700px] mx-auto bg-left-panel">
                 <div className="hidden md:fixed top-0 left-0 w-full h-36 bg-green-primary dark:bg-transparent -z-30" />
-                <AsyncLeftPanel />
+                <LeftPanel clerkId={clerk.clerkId} />
             </div>
-            <MobileLeftPanel />
+            <MobileLeftPanel clerkId={clerk.clerkId} />
             <div className="h-full md:pl-1/3">{children}</div>
         </div>
     )
