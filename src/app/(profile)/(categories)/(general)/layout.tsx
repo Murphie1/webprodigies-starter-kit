5b3d36a3041f9ff93c0956e2e7aec0fa8d8f4037
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-02 13:12:33 +0100

    Update layout.tsx

--- src/app/(profile)/(categories)/(general)/layout.tsx
+++ src/app/(profile)/(categories)/(general)/layout.tsx
@@ -7,7 +7,7 @@ type Props = {
 const GenLayout = ({ children }: Props) => {
     return (
         <div>
-            <div style={{ paddingBottom: "70px" }}>{children}</div>
+            <div className="pb-[75px]">{children}</div>
             <BottomBar />
         </div>
     )
