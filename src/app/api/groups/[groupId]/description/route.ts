1b95e4fcfceaa7b8f5b92b6af405d6142c353c4e
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-03 21:35:19 +0100

    Create route.ts

---
+++ src/app/api/groups/[groupId]/description/route.ts
@@ -0,0 +1,32 @@
+
+import { client } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { groupId: string; } }
+) {
+  try {
+    const { userId } = auth();
+    const { groupId } = params;
+    const {  ...values } = await req.json();
+
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
+    const ownGroup = await client.group.findUnique({
+      where: { id: groupId, userId },
+    });
+
+    if (!ownCourse) return new NextResponse("Unauthorized", { status: 401 });
+
+    const groupUpdate = await client.group.update({
+      where: { id: groupId },
+      data: { ...values },
+    });
+  } catch (error) {
+    console.log("GROUP_ID", error);
+    new NextResponse("Internal Error", { status: 500 });
+  }
+  }
