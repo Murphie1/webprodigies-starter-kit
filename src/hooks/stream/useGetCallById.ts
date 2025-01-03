be5931af3d74a1d5a865783efb65335f1a422a3d
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-03 17:22:45 +0100

    Update useGetCallById.ts

--- src/hooks/stream/useGetCallById.ts
+++ src/hooks/stream/useGetCallById.ts
@@ -1,48 +1,45 @@
 import { useEffect, useState } from 'react';
-import { Call, useStreamVideoClient, StreamVideoClient } from '@stream-io/video-react-sdk';
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
 import { useUser } from '@clerk/nextjs';
-
 import { tokenProvider } from '@/actions/stream.actions';
-
-
-const API_KEY = process.env.NEXT_PUBLIC_STREAM_API_KEY;
-
 
 export const useGetCallById = (id: string | string[]) => {
   const { user, isLoaded } = useUser();
+  const client = useStreamVideoClient();
   const [call, setCall] = useState<Call>();
-  const [videoClient, setVideoClient] = useState<StreamVideoClient>();
   const [isCallLoading, setIsCallLoading] = useState(true);
-
-  
-useEffect(() => {
-    if (!isLoaded || !user) return;
-    if (!API_KEY) throw new Error('Stream API key is missing');
-
-    const client = new StreamVideoClient({
-      apiKey: API_KEY,
-      user: {
-        id: user?.id,
-        name: user?.firstName || user?.lastName || user?.username || user?.id,
-        image: user?.imageUrl,
-      },
-      tokenProvider,
-    });
-
-    setVideoClient(client);
-  }, [user, isLoaded]);
-  //const client = useStreamVideoClient();
+  const [isUserConnected, setIsUserConnected] = useState(false); // Track if the user is connected
 
   useEffect(() => {
-    if (!videoClient) return;
-    
+    if (!isLoaded || !user || !client || isUserConnected) return;
+
+    const initializeClient = async () => {
+      try {
+        await client.connectUser(
+          {
+            id: user.id,
+            name: user.firstName || user.lastName || user.username || user.id,
+            image: user.imageUrl,
+          },
+          tokenProvider
+        );
+        setIsUserConnected(true); // Mark the user as connected
+      } catch (error) {
+        console.error('Error connecting user:', error);
+      }
+    };
+
+    initializeClient();
+  }, [user, isLoaded, client, isUserConnected]);
+
+  useEffect(() => {
+    if (!client || !id || !isUserConnected) return;
+
     const loadCall = async () => {
       try {
-        // https://getstream.io/video/docs/react/guides/querying-calls/#filters
-        const { calls } = await videoClient.queryCalls({ filter_conditions: { id } });
+        const { calls } = await client.queryCalls({ filter_conditions: { id } });
 
         if (calls.length > 0) setCall(calls[0]);
-
         setIsCallLoading(false);
       } catch (error) {
         console.error(error);
@@ -51,7 +48,7 @@ useEffect(() => {
     };
 
     loadCall();
-  }, [videoClient, id]);
+  }, [client, id, isUserConnected]);
 
   return { call, isCallLoading };
 };
