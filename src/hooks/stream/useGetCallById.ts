3f1e4653fc4471cab8d92cc105bbccb58e0e237f
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-03 17:07:03 +0100

    Update useGetCallById.ts

--- src/hooks/stream/useGetCallById.ts
+++ src/hooks/stream/useGetCallById.ts
@@ -1,19 +1,45 @@
 import { useEffect, useState } from 'react';
-import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
+import { Call, useStreamVideoClient, StreamVideoClient } from '@stream-io/video-react-sdk';
+import { useUser } from '@clerk/nextjs';
+
+import { tokenProvider } from '@/actions/stream.actions';
+
+
+const API_KEY = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+
 
 export const useGetCallById = (id: string | string[]) => {
+  const { user, isLoaded } = useUser();
   const [call, setCall] = useState<Call>();
+  const [videoClient, setVideoClient] = useState<StreamVideoClient>();
   const [isCallLoading, setIsCallLoading] = useState(true);
 
-  const client = useStreamVideoClient();
+  
+useEffect(() => {
+    if (!isLoaded || !user) return;
+    if (!API_KEY) throw new Error('Stream API key is missing');
+
+    const client = new StreamVideoClient({
+      apiKey: API_KEY,
+      user: {
+        id: user?.id,
+        name: user?.firstName || user?.lastName || user?.username || user?.id,
+        image: user?.imageUrl,
+      },
+      tokenProvider,
+    });
+
+    setVideoClient(client);
+  }, [user, isLoaded]);
+  //const client = useStreamVideoClient();
 
   useEffect(() => {
-    if (!client) return;
+    if (!videoClient) return;
     
     const loadCall = async () => {
       try {
         // https://getstream.io/video/docs/react/guides/querying-calls/#filters
-        const { calls } = await client.queryCalls({ filter_conditions: { id } });
+        const { calls } = await videoClient.queryCalls({ filter_conditions: { id } });
 
         if (calls.length > 0) setCall(calls[0]);
 
@@ -25,7 +51,7 @@ export const useGetCallById = (id: string | string[]) => {
     };
 
     loadCall();
-  }, [client, id]);
+  }, [videoClient, id]);
 
   return { call, isCallLoading };
 };
