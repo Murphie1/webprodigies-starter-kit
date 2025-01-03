a2bd3d74897929bd1654c66ab674ce7d3f948320
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-03 19:06:52 +0100

    Update layout.tsx

--- src/app/(profile)/(categories)/(general)/socials/conferencing/layout.tsx
+++ src/app/(profile)/(categories)/(general)/socials/conferencing/layout.tsx
@@ -1,11 +1,13 @@
-
+import StreamVideoProvider from "@/providers/StreamClientProvider";
 import { StreamClientProvider } from '@/providers/StreamProvider';
 
 export default function ConferenceLayout({ children }: { children: React.ReactNode }) {
   return (
       <body>
         <StreamClientProvider>
+          <StreamVideoProvider>
           {children}
+            </StreamVideoProvider>
         </StreamClientProvider>
       </body>
   );
