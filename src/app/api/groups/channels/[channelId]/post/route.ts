42ebfe4e449a1e43223868d096fba2fde4995709
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-04 09:16:09 +0100

    Update route.ts

--- src/app/api/groups/channels/[channelId]/post/route.ts
+++ src/app/api/groups/channels/[channelId]/post/route.ts
@@ -23,7 +23,7 @@ export async function POST(
 
         const post = await client.post.create({
             data: {
-                channeld,
+                channelId,
                 title,
                 content,
                 video,
