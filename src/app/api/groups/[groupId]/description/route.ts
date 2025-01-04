80ffd36930a28e42c994a87bf8ea6b51a97500dc
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-04 09:28:44 +0100

    Update route.ts

--- src/app/api/groups/[groupId]/description/route.ts
+++ src/app/api/groups/[groupId]/description/route.ts
@@ -2,24 +2,24 @@
 import { client } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-
+import { onAuthenticatedUser } from "@/actions/auth"
 
 export async function PATCH(
   req: Request,
   { params }: { params: { groupId: string; } }
 ) {
   try {
-   // const { userId } = auth();
+    const user = await onAuthenticatedUser();
     const { groupId } = params;
     const { ...values } = await req.json();
 
-    //if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+    if (!user || !user.id) return new NextResponse("Unauthorized", { status: 401 });
 
-   // const ownGroup = await client.group.findUnique({
-    //  where: { id: groupId, userId, },
-   // });
+   const ownGroup = await client.group.findUnique({
+      where: { id: groupId, user?.id, },
+    });
 
-   // if (!ownGroup) return new NextResponse("Unauthorized", { status: 401 });
+    if (!ownGroup) return new NextResponse("Unauthorized", { status: 401 });
 
     const groupUpdate = await client.group.update({
       where: { id: groupId },
