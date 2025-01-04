a30f0461770d7b79d80f9b5a1a3d1e73b387c026
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-04 10:36:58 +0100

    Update route.ts

--- src/app/api/groups/channels/[channelId]/post/route.ts
+++ src/app/api/groups/channels/[channelId]/post/route.ts
@@ -1,39 +1,46 @@
-import { onAuthenticatedUser } from "@/actions/auth"
-import { client } from "@/lib/prisma"
-import { currentUser } from "@clerk/nextjs/server"
-import { redirect } from "next/navigation"
-import { NextResponse } from "next/server"
+import { onAuthenticatedUser } from "@/actions/auth";
+import { client } from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { NextResponse } from "next/server";
+import { v4 as uuidv4 } from "uuid";
 
 export async function POST(
   req: Request,
   { params }: { params: { channelId: string } }
 ) {
-    try {
-       const { channelId } = params;
-        const clerk = await currentUser()
-        if (!clerk) {
-            return redirect("/sign-in")
-        }
-        const { title, image, content, video } = await req.json()
-        const user = await onAuthenticatedUser()
-
-        if (!user || user.id ) {
-            return new NextResponse("userless", { status: 401 })
-        }
-
-        const post = await client.post.create({
-            data: {
-                channelId,
-                title,
-                content,
-                video,
-                image,
-                authorId: user.id!,
-                htmlContent: content,
-            },
-        })
-    } catch (error) {
-        console.log("[POST_POST]", error)
-        return new NextResponse("Internal Error", { status: 500 })
+  try {
+    const { channelId } = params;
+    const clerk = await currentUser();
+    if (!clerk) {
+      return redirect("/sign-in");
     }
-}
+    const { title, image, content, video } = await req.json();
+    const user = await onAuthenticatedUser();
+
+    if (!user || !user.id) {
+      return new NextResponse("userless", { status: 401 });
+    }
+
+    // Generate a unique ID for the post
+    const postId = uuidv4();
+
+    const post = await client.post.create({
+      data: {
+        id: postId, // Use the generated ID
+        channelId,
+        title,
+        content,
+        video,
+        image,
+        authorId: user.id!,
+        htmlContent: content,
+      },
+    });
+
+    return NextResponse.json(post);
+  } catch (error) {
+    console.log("[POST_POST]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+    }
