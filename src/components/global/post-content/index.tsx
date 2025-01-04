575d23dbadc3acbdc1f66acec84a1a1a06ca0f32
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-04 08:47:06 +0100

    Update index.tsx

--- src/components/global/post-content/index.tsx
+++ src/components/global/post-content/index.tsx
@@ -1,5 +1,5 @@
 "use client"
-//import BlockTextEditor from "@/components/global/rich-text-editor"
+
 import { Button } from "@/components/ui/button"
 import {
     Form,
@@ -11,42 +11,33 @@ import {
 } from "@/components/ui/form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Input } from "@/components/ui/input"
-import { useEffect, useState } from "react"
+import { Textarea } from "@/components/ui/textarea" // Assuming this exists
 import { useForm } from "react-hook-form"
 import * as z from "zod"
 import axios from "axios"
 import { useRouter } from "next/navigation"
 import { FileUpload } from "@/components/file-upload"
-
-import { useCreateChannelPost } from "@/hooks/channels"
-
 import { Upload } from "lucide-react"
 
 type PostContentProps = {
     channelid: string
 }
 
-
-    const formSchema = z.object({
+const formSchema = z.object({
+    title: z.string().optional(), // Title is optional
     content: z.string().min(1, {
         message: "Your Post cannot be empty.",
     }),
-  //  imageUrl: z.string().min(1, {
-      //  message: "Having an image is also quite important you know.",
-    //}),
+    image: z.string().optional(),
+    video: z.string().optional(),
 })
 
-
 export const PostContent = ({ channelid }: PostContentProps) => {
-    const [isMounted, setIsMounted] = useState(false)
     const [imageOpen, setImageOpen] = useState(false)
     const [videoOpen, setVideoOpen] = useState(false)
-    
+
     const router = useRouter()
 
-    useEffect(() => {
-        setIsMounted(true)
-    }, [])
     const form = useForm({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -62,130 +53,124 @@ export const PostContent = ({ channelid }: PostContentProps) => {
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
             await axios.post(`/api/groups/channels/${channelid}/post`, values)
-
             form.reset()
             router.refresh()
             window.location.reload()
         } catch (error) {
-            console.log(error)
+            console.error(error)
         }
     }
 
-    if (!isMounted) {
-        return null
-   }
     return (
         <Form {...form}>
-                    <form
-                        onSubmit={form.handleSubmit(onSubmit)}
-                        className="space-y-8"
-                    >
-                        <div className="space-y-8 px-6">
-                            <FormField
-                                control={form.control}
-                                name="title"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
-                                            Title
-                                        </FormLabel>
-                                        <FormControl>
-                                            <Input
-                                                disabled={isLoading}
-                                                className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0 dark:text-white dark:border-2 dark:border-themeGray"
-                                                placeholder="e.g Announcement"
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-              control={form.control}
-              name="content"
-              render={({ field }) => (
-                <FormItem>
-                    <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
-                                            Content
-                                        </FormLabel>
-                  <FormControl>
-                    <Textarea
-                      disabled={isLoading}
-                      {...field}
-                      placeholder="e.g. 'This post is about...'"
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <div className="space-y-8 px-6">
+                    {/* Title Field */}
+                    <FormField
+                        control={form.control}
+                        name="title"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                                    Title (Optional)
+                                </FormLabel>
+                                <FormControl>
+                                    <Input
+                                        disabled={isLoading}
+                                        {...field}
+                                        placeholder="e.g. 'Announcement'"
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
                     />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-                            {imageOpen ? ( 
-                        <div className="flex items-center justify-center text-center">
-                                <FormField
-                                    control={form.control}
-                                    name="image"
-                                    render={({ field }) => (
-                                        <FormItem>
-                                            <FormControl>
-                                                <FileUpload
-                                                    endpoint="postImage"
-                                                    value={field.value}
-                                                    onChange={field.onChange}
-                                                />
-                                            </FormControl>
-                                            <FormMessage />
-                                        </FormItem>
-                                    )}
-                                />
-                            </div> ) : ( 
-                        <Button type="button" className="self-end rounded-2xl bg-themeBlack text-white dark:bg-white dark:text-black flex gap-x-2">
-                <Upload onClick={() => setImageOpen(true)} />
-                Add an Image
-            </Button>
+                    {/* Content Field */}
+                    <FormField
+                        control={form.control}
+                        name="content"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                                    Content
+                                </FormLabel>
+                                <FormControl>
+                                    <Textarea
+                                        disabled={isLoading}
+                                        {...field}
+                                        placeholder="e.g. 'This post is about...'"
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
                         )}
-                            {videoOpen ? ( 
-                        <div className="flex items-center justify-center text-center">
-                                <FormField
-                                    control={form.control}
-                                    name="video"
-                                    render={({ field }) => (
-                                        <FormItem>
-                                            <FormControl>
-                                                <FileUpload
-                                                    endpoint="postVideo"
-                                                    value={field.value}
-                                                    onChange={field.onChange}
-                                                />
-                                            </FormControl>
-                                            <FormMessage />
-                                        </FormItem>
-                                    )}
-                                />
-                            </div> ) : ( 
-                        <Button type="button" className="self-end rounded-2xl bg-themeBlack text-white dark:bg-white dark:text-black flex gap-x-2">
-                <Upload onClick={() => setVideoOpen(true)} />
-                Add a Video
-            </Button>
-                        )}
-                        </div>
-                        
-                                <div className="bg-transparent px-6 py-4">
-                                            <Button
-                                                disabled={isLoading}
-                                                variant="ghost"
-                                                type="submit"
-                                            >
-                                                Post
-                                            </Button>
-                                        <div>
-                    </form>
-                </Form>
-        )
-    }
-
-
-
+                    />
+                    {/* Image Upload */}
+                    {imageOpen ? (
+                        <FormField
+                            control={form.control}
+                            name="image"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <FileUpload
+                                            endpoint="postImage"
+                                            value={field.value}
+                                            onChange={field.onChange}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    ) : (
+                        <Button
+                            type="button"
+                            className="self-end rounded-2xl bg-themeBlack text-white dark:bg-white dark:text-black flex gap-x-2"
+                            onClick={() => setImageOpen(true)}
+                        >
+                            <Upload />
+                            Add an Image
+                        </Button>
+                    )}
+                    {/* Video Upload */}
+                    {videoOpen ? (
+                        <FormField
+                            control={form.control}
+                            name="video"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <FileUpload
+                                            endpoint="postVideo"
+                                            value={field.value}
+                                            onChange={field.onChange}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    ) : (
+                        <Button
+                            type="button"
+                            className="self-end rounded-2xl bg-themeBlack text-white dark:bg-white dark:text-black flex gap-x-2"
+                            onClick={() => setVideoOpen(true)}
+                        >
+                            <Upload />
+                            Add a Video
+                        </Button>
+                    )}
+                </div>
+                <div className="bg-transparent px-6 py-4">
+                    <Button disabled={isLoading} variant="ghost" type="submit">
+                        Post
+                    </Button>
+                </div>
+            </form>
+        </Form>
+    )
+               }
 
 
 
