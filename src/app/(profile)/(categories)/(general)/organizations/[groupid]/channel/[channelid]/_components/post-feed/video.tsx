da443b559d8eaf8611e92fde57b06d2c30aac627
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-05 11:14:36 +0100

    Update video.tsx

--- src/app/(profile)/(categories)/(general)/organizations/[groupid]/channel/[channelid]/_components/post-feed/video.tsx
+++ src/app/(profile)/(categories)/(general)/organizations/[groupid]/channel/[channelid]/_components/post-feed/video.tsx
@@ -4,14 +4,12 @@ import {
   DialogDescription,
   DialogTrigger,
 } from "@/components/ui/dialog";
-//import { Button } from "@/components/ui/button";
-//import Image from "next/image";
 import ReactPlayer from "react-player";
 
-export const VideoModal = (
-  video
+export const VideoModal = ({
+  video,
 }: {
-  video?: string | null;
+  video?: string | null | undefined;
 }) => {
   return (
     <Dialog>
@@ -19,14 +17,14 @@ export const VideoModal = (
       <DialogTrigger asChild>
         <div className="cursor-pointer">
           {video ? (
-          <div className="w-full">
-              <ReactPlayer url={video || ""} controls width="100%" />
+            <div className="w-full">
+              <ReactPlayer url={video} controls width="100%" height="100%" />
             </div>
           ) : (
             <div className="w-full">
               <p className="text-center text-gray-500 dark:text-gray-400">
-              No content available
-            </p>
+                No content available
+              </p>
             </div>
           )}
         </div>
@@ -34,7 +32,7 @@ export const VideoModal = (
 
       {/* Modal Content */}
       <DialogContent className="w-full max-w-screen-md p-4 dark:bg-gray-800 bg-white rounded-lg shadow-lg">
-        <DialogDescription className="relative flex items-center justify-center h-[350px] sm:h-[550px]">
+        <DialogDescription className="relative w-full h-full flex items-center justify-center">
           {video ? (
             <ReactPlayer url={video} controls width="100%" height="100%" />
           ) : (
