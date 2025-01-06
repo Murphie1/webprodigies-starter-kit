a0736b9f2501f4fc75416501fc933812a056ded0
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-06 16:05:01 +0100

    Create Header.tsx

---
+++ src/components/library/Header.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Search from "./Search";
+import FileUploader from "./FileUploader";
+//import { signOutUser } from "@/lib/actions/user.actions";
+
+const Header = ({
+  userId,
+  accountId,
+}: {
+  userId: string;
+  accountId: string;
+}) => {
+  return (
+    <header className="header">
+      <Search />
+      <div className="header-wrapper">
+        <FileUploader ownerId={userId} accountId={accountId} />
+          <Button className="sign-out-button">
+            <Image
+              src="/assets/icons/logout.svg"
+              alt="logo"
+              width={24}
+              height={24}
+              className="w-6"
+            />
+          </Button>
+      </div>
+    </header>
+  );
+};
+export default Header;
