2816aebe74177c91dc71da2b46c3135fb7f0ecb0
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-06 14:22:33 +0100

    Create page.tsx

---
+++ src/app/(profile)/(categories)/(general)/organizations/[groupid]/library/page.tsx
@@ -0,0 +1,28 @@
+import { onAuthenticatedUser } from "@/actions/auth"
+import { redirect } from "next/navigation"
+import { authenticateUser } from "@/lib/actions/user.actions"
+import { currentUser } from "@clerk/nextjs/server"
+
+
+const LibraryPage = async () => {
+  const user = await onAuthenticatedUser()
+  if (!user) redirect("/")
+
+  const clerk = await currentUser()
+  if (!clerk) redirect("/sign-in")
+
+  const appwriteUser = await authenticateUser({
+    clerkId: user.clerkId || clerk.id,
+    fullName: user.username || `${clerk.firstName + clerk.lastName}`,
+    email: user.email || clerk.emailAddresses[0]?.emailAddress,
+    avatar: user.image || clerk.imageUrl,
+  })
+  
+    return 
+      <div>
+        <p>{user.username}</p>
+        library
+      </div>
+}
+
+export default LibraryPage
