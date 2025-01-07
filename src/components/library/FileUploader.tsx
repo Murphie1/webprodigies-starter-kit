4620f99358eeaac28a413e26cce2637fce91316c
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-07 15:51:50 +0100

    Update FileUploader.tsx

--- src/components/library/FileUploader.tsx
+++ src/components/library/FileUploader.tsx
@@ -44,7 +44,7 @@ const FileUploader = ({ ownerId, accountId, className }: Props) => {
           });
         }
 
-        return uploadGroupFile({ file, ownerId: "123", clerkId: "123", groupId: "123", accountId: "123", path }).then(
+        return uploadGroupFile({ file: "123", ownerId: "123", clerkId: "123", groupId: "123", accountId: "123", path }).then(
           (uploadedFile) => {
             if (uploadedFile) {
               setFiles((prevFiles) =>
