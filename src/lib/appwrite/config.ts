86e5901bf0c4cb9413337fbc3be7e2b4e1c02c22
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-08 15:04:06 +0100

    Update config.ts

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -2,6 +2,7 @@ export const appwriteConfig = {
   endpointUrl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
   projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT!,
   databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE!,
+  foldersCollectionId: process.env.NEXT_PUBLIC_APPWRITE_FOLDERS_COLLECTION!,
   usersCollectionId: process.env.NEXT_PUBLIC_APPWRITE_USERS_COLLECTION!,
   filesCollectionId: process.env.NEXT_PUBLIC_APPWRITE_FILES_COLLECTION!,
   bucketId: process.env.NEXT_PUBLIC_APPWRITE_BUCKET!,
