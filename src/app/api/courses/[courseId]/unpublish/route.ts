ca487a839d621b29eff74afc5e8fae4a787991b9
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-11 18:01:04 +0100

    Create route.ts

---
+++ src/app/api/courses/[courseId]/unpublish/route.ts
@@ -0,0 +1,36 @@
+import { client } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { courseId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { courseId } = params.courseId;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const course = await client.course.findUnique({
+      where: { id: courseId, userId },
+    });
+    if (!course) return new NextResponse("Not Found", { status: 404 });
+
+    const unpublishedCourse = await client.course.update({
+      where: {
+        id: courseId,
+        userId,
+      },
+      data: {
+        isPublished: false,
+      },
+    });
+    return NextResponse.json(unpublishedCourse);
+  } catch (error) {
+    console.log("[COURSE_ID_UNPUBLISH]", error);
+    return new NextResponse("Internal Error!", { status: 500 });
+  }
+        }
