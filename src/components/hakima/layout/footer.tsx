b03b1803196a9f2de1f63f6866fbbd269e361c96
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-25 13:18:24 +0100

    Create footer.tsx

---
+++ src/components/hakima/layout/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <div className=" w-full p-1 text-xs flex flex-row justify-center">
+      <p className="text-xs text-zinc-500/50">
+        LLMs can make mistakes, please verify any info.
+      </p>
+    </div>
+  );
+};
