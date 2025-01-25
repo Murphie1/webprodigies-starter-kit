9a83c2385264e567c35dfa971a254311ebdf022d
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-25 11:03:57 +0100

    Create adding-soon.tsx

---
+++ src/components/hakima/adding-soon.tsx
@@ -0,0 +1,12 @@
+import { Badge } from "@/components/ui/badge";
+
+export const AddingSoon = () => {
+  return (
+    <Badge
+      className="bg-blue-400/50 text-blue-200 dark:text-blue-800 rounded-full font-normal"
+      variant={"secondary"}
+    >
+      Adding soon
+    </Badge>
+  );
+};
