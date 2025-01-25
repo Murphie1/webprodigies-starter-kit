6c69c5e8f747204641c6e2366ee7b577fd2faa9f
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-25 13:56:49 +0100

    Create layout.tsx

---
+++ src/app/(profile)/hakima/layout.tsx
@@ -0,0 +1,53 @@
+import { TooltipProvider } from "@/components/ui/tooltip";
+import type { Viewport } from "next";
+import { MainLayout } from "@/components/hakima/layout/main-layout";
+import React from "react";
+import {
+AssistantsProvider,
+  ChatProvider,
+  FiltersProvider,
+  PromptsProvider,
+  ConfirmProvider,
+  PreferenceProvider,
+  ReactQueryProvider,
+  SessionsProvider,
+  SettingsProvider,
+} from "@/context";
+
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+            <TooltipProvider>
+              <ConfirmProvider>
+                <PreferenceProvider>
+                  <SessionsProvider>
+                    <SettingsProvider>
+                      <ChatProvider>
+                        <FiltersProvider>
+                         <AssistantsProvider>
+                          <PromptsProvider>
+                           <MainLayout>
+                           {children}
+                           </MainLayout>
+                         </PromptsProvider>
+                       </AssistantsProvider>
+                      </FiltersProvider>
+                     </ChatProvider>
+                    </SettingsProvider>
+                  </SessionsProvider>
+                </PreferenceProvider>
+              </ConfirmProvider>
+            </TooltipProvider>
+  );
+  }
