1ba9d52158a4ad703e0aa8306382cce0cb1116f6
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-26 20:13:00 +0100

    Update greeting-bubble.tsx

--- src/components/hakima/greeting-bubble.tsx
+++ src/components/hakima/greeting-bubble.tsx
@@ -2,20 +2,20 @@ import { TModel } from "@/hooks/hakima/use-model-list";
 import { BotAvatar } from "./bot-avatar";
 import { motion } from "framer-motion";
 import { REVEAL_ANIMATION_VARIANTS } from "@/hooks/hakima/use-mdx";
-import { Tooltip } from "../ui/tooltip";
+import { Tooltip } from "./tooltip";
 
 export type TGreetingBubble = {
-  bot: TBot;
+  bot: TModel;
 };
 
 export const GreetingBubble = ({ bot }: TGreetingBubble) => {
   return (
     <div className="flex flex-col md:flex-row gap-2 mt-6 w-full">
-      <div className="px-0 md:px-3 py-1">
+  {/*<div className="px-0 md:px-3 py-1">
         <Tooltip content={bot?.name}>
           <BotAvatar size={"small"} name={bot?.name} avatar={bot?.avatar} />
         </Tooltip>
-      </div>
+      </div>*/}
       <div className="rounded-2xl w-full flex flex-col items-start">
         <div className="py-1.5 w-full">
           <p className="text-sm md:text-base">
@@ -25,7 +25,7 @@ export const GreetingBubble = ({ bot }: TGreetingBubble) => {
               animate={"visible"}
               initial={"hidden"}
             >
-              {bot.greetingMessage}
+                               {bot?.name} {/* {bot.greetingMessage}*/}
             </motion.span>
           </p>
         </div>
