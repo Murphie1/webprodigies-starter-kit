09e412e4e2447445d4f0274c09066436f21b9efd
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-26 16:51:19 +0100

    Update chat.tsx

--- src/context/chat.tsx
+++ src/context/chat.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useToast } from "@/components/ui/use-toast";
-import { TToolResponse, defaultPreferences, useTools } from "@/hooks";
+import { TToolResponse, defaultPreferences, useTools } from "@/hooks/hakima";
 import {
   TAssistant,
   TChatMessage,
