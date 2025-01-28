4d0107e8155f8622c806461b31f876396dd6fb51
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-28 17:56:27 +0100

    Create sidebar.tsx

---
+++ src/app/(profile)/teacher/_components/sidebar.tsx
@@ -0,0 +1,28 @@
+//SideBar
+
+import { BookOpen } from "lucide-react"
+import { SidebarRoutes } from "./sidebar-routes"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+
+export const SideBar = () => {
+    return (
+        <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm dark:bg-black">
+            <div className="p-6 flex-1 space-x-2">
+                <BookOpen />
+                YouLearn
+            </div>
+            <div className="grid grid-cols-2 pb-2 px-2">
+                <Link href="/">
+                    <Button className="rounded-md">Profiles</Button>
+                </Link>
+                <Link href="/home">
+                    <Button className="rounded-md">Back Home</Button>
+                </Link>
+            </div>
+            <div className="flex flex-col w-full">
+                <SidebarRoutes />
+            </div>
+        </div>
+    )
+          }
