d428bc6c25280db532831e36e87bbc4d420d520b
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-29 15:53:45 +0100

    Update page.tsx

--- src/app/(profile)/hakima/page.tsx
+++ src/app/(profile)/hakima/page.tsx
@@ -1,16 +1,23 @@
-"use client";
+//"use client";
 
 import Spinner from "@/components/hakima/loading-spinner";
-import { useSessionsContext } from "@/context";
-import { useEffect } from "react";
+//import { useSessionsContext } from "@/context";
+import { Navbar } from "@/components/hakima/layout/navbar";
+//import { useEffect } from "react";
 
 //export const dynamic = 'force-dynamic';
 //export const revalidate = 0;
 //export const fetchCache = 'force-no-store';
-export const runtime = 'edge'; // Optional: forces edge runtime
+//export const runtime = 'edge'; // Optional: forces edge runtime
 
 export default function Home() {
-  const { createSession } = useSessionsContext();
+  return (
+    <main className="flex flex-col gap-2 h-[100dvh] w-screen items-center justify-center">
+      <Navbar />
+    </main>
+  );
+}
+  {/*const { createSession } = useSessionsContext();
 
   useEffect(() => {
     createSession({ redirect: true });
@@ -22,3 +29,4 @@ export default function Home() {
     </main>
   );
 }
+*/}
