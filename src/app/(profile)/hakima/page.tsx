66c90b18f34f0a1be15e14eb6ec577b71b8341a3
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-29 09:21:47 +0100

    Update page.tsx

--- src/app/(profile)/hakima/page.tsx
+++ src/app/(profile)/hakima/page.tsx
@@ -7,7 +7,7 @@ import { useEffect } from "react";
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 export const fetchCache = 'force-no-store';
-export const runtime = 'edge'; // Optional: forces edge runtime
+//export const runtime = 'edge'; // Optional: forces edge runtime
 
 export default function Home() {
   const { createSession } = useSessionsContext();
