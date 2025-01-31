18d8b1b81dfc669969c305b233e38bc75866f218
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-01-31 21:30:21 +0100

    Create mobile-sidebar.tsx

---
+++ src/app/(profile)/teacher/_components/mobile-sidebar.tsx
@@ -0,0 +1,19 @@
+import { AlignLeft } from "lucide-react"
+import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet"
+import { SideBar } from "./sidebar"
+
+export const MobileSideBar = () => {
+    return (
+        <Sheet>
+            <SheetTrigger
+                type="button"
+                className="md:hidden pr-4 hover:opacity-75 transition"
+            >
+                <AlignLeft />
+            </SheetTrigger>
+            <SheetContent side="left" className="p-0 bg-white dark:bg-black">
+                <SideBar />
+            </SheetContent>
+        </Sheet>
+    )
+      }
