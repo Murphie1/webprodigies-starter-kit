e4376421bb01c882e8fd18ff5b44236ab26371b2
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-02-01 12:14:56 +0100

    Update page.tsx

--- src/app/(profile)/clarion/page.tsx
+++ src/app/(profile)/clarion/page.tsx
@@ -1,7 +1,12 @@
-import { Chat } from '@/components/chat'
+//"use client";
+
+import Spinner from "@/components/hakima/loading-spinner";
+import { redirect } from "next/navigation"
 import { generateId } from 'ai'
 
 export default function Page() {
   const id = generateId()
-  return <Chat id={id} />
+  if (id) redirect(`/clarion/${id}`)
+  
+  return <Spinner />
 }
