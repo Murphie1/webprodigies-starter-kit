7030062e53db3d9626506cf409b46a542f6b55b9
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-02-02 15:23:41 +0100

    Update route.ts

--- src/app/api/stripe/connect/route.ts
+++ src/app/api/stripe/connect/route.ts
@@ -5,7 +5,7 @@ import Stripe from "stripe"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
     typescript: true,
-    apiVersion: "2024-09-30.acacia",
+    apiVersion: "2025-01-27.acacia",
 })
 
 export async function GET(req: NextRequest) {
