decf9f46c183c30e3d95d3b9e3c30eb53f13a8db
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-02-02 15:22:46 +0100

    Update payments.ts

--- src/actions/payments.ts
+++ src/actions/payments.ts
@@ -5,7 +5,7 @@ import { onAuthenticatedUser } from "./auth"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
     typescript: true,
-    apiVersion: "2024-09-30.acacia",
+    apiVersion: "2025-01-27.acacia",
 })
 
 export const onGetStripeClientSecret = async () => {
