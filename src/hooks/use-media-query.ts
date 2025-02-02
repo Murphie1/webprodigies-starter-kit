509eeff8da38b620ac3387d341938570f53fe9b1
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-02-02 13:05:37 +0100

    Create use-media-query.ts

---
+++ src/hooks/use-media-query.ts
@@ -0,0 +1,27 @@
+"use client"
+
+import { useState, useEffect } from "react";
+
+const useMediaQuery = (query: string) => {
+  const [matches, setMatches] = useState<boolean>(false);
+
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia(query);
+
+    const updateMatches = () => {
+      setMatches(mediaQueryList.matches);
+    };
+
+    mediaQueryList.addEventListener("change", updateMatches);
+
+    // Initial check
+    updateMatches();
+
+    // Clean up listener on component unmount
+    return () => {
+      mediaQueryList.removeEventListener("change", updateMatches);
+    };
+  }, [query]);
+
+  return matches;
+};
