b8814181e4c684a0c8dd15efa08b3d9444e9d160
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-02-02 14:37:25 +0100

    Update layout.tsx

--- src/app/(profile)/(categories)/(general)/socials/conferencing/(home)/layout.tsx
+++ src/app/(profile)/(categories)/(general)/socials/conferencing/(home)/layout.tsx
@@ -1,6 +1,7 @@
 import { NavBar } from "@/components/navbar"
 import { SideBar } from "@/components/sidebar"
 import { Phone, Plus } from "lucide-react"
+import Link from "next/link"
 
 type Props = {
     children: React.ReactNode
@@ -22,18 +23,18 @@ const ConferenceLayout = ({ children }: Props) => {
             {/* Main Content */}
             <main className="h-full flex flex-col md:pl-64 pt-[50px] md:pt-[75px]">
                 <div className="flex justify-center mx-1 space-x-2 md:space-x-8 md:mx-auto pt-3 pb-6">
-                    <a
+                    <Link 
                         href="/socials/conferencing/calls"
                         className="px-4 py-2 flex flex-1 text-white bg-blue-600 rounded-lg hover:bg-blue-700"
                     >
                        <Phone size={25} /> Your Calls
-                    </a>
-                    <a
+                    </Link>
+                    <Link
                         href="/socials/conferencing/room"
                         className="px-4 py-2 flex flex-1 text-white bg-gray-600 rounded-lg hover:bg-gray-700"
                     >
                         <Plus size={25} /> Create a Call
-                    </a>
+                    </Link>
                 </div>
                 <div className="px-6 pb-6 flex max-md:pb-14 sm:px-14">
                 {children}
