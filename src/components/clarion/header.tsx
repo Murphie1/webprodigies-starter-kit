9a2477efb1ec8a224965506bcd75b56f2cf336a8
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-02-02 14:33:26 +0100

    Update header.tsx

--- src/components/clarion/header.tsx
+++ src/components/clarion/header.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from "next/link"
 import { ModeToggle } from '@/components/mode-toggle'
 import { IconLogo } from './ui/icons'
 import { cn } from '@/lib/utils'
@@ -8,10 +9,10 @@ export const Header: React.FC = async () => {
   return (
     <header className="fixed w-full p-1 md:p-2 flex justify-between items-center z-10 backdrop-blur md:backdrop-blur-none bg-background/80 md:bg-transparent">
       <div>
-        <a href="/">
+        <Link href="/">
           <IconLogo className={cn('w-5 h-5')} />
           <span className="sr-only">Clarion: Virtual Tutor</span>
-        </a>
+        </Link>
       </div>
       <div className="flex gap-0.5">
         <ModeToggle />
