5b132ba65ed9cfb92015d14a50b35e3252de1a79
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-02-03 18:58:30 +0100

    Update duckduckgo.ts

--- src/tools/duckduckgo.ts
+++ src/tools/duckduckgo.ts
@@ -10,7 +10,7 @@ const duckduckGoTool = (args: TToolArg) => {
   return new DynamicStructuredTool({
     name: "web_search",
     description:
-      "A search engine optimized for comprehensive, accurate, and trusted results. Useful for when you need to answer questions about current events. Input should be a search query. Don't use tool if already used it to answer the question.",
+      "A search engine optimized for comprehensive, accurate, and trusted results. Useful for when you need to answer questions about current events. Whenever you are asked about current or the latest events do not tell the user you don't have access to it, use this search to get information and then always make sure to remind the user in the end that this information was from your search so it may not be accurate, and that should try to verify this if they can. Input should be a search query. Don't use tool if already used it to answer the question.",
     schema: webSearchSchema,
     func: async ({ input }, runManager) => {
       try {
