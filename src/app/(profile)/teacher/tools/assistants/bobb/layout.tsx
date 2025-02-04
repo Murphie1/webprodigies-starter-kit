935042dab109bf2da86f6f06e6e2b932756e4326
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-02-04 17:14:57 +0100

    Create layout.tsx

---
+++ src/app/(profile)/teacher/tools/assistants/bobb/layout.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Sidebar from "@/components/bobb/Sidebar";
+import Header from "@/components/bobb/Header";
+import { NavigationProvider } from "@/lib/bobb/context/navigation";
+
+import { auth } from "@clerk/nextjs/server";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/");
+  }
+  
+  return (
+    <NavigationProvider>
+      <div className="flex h-screen">
+          <Sidebar clerkId={userId} />
+        <div className="flex-1 flex flex-col min-w-0">
+          <Header />
+          <main className="flex-1 overflow-y-auto">{children}</main>
+        </div>
+      </div>
+    </NavigationProvider>
+  );
+          }
