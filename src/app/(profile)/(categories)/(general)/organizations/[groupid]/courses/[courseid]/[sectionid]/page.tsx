5d83010382b40b03e9714ad4fc4286b1a0ee1b8a
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-02-23 20:06:36 +0100

    Update page.tsx

--- src/app/(profile)/(categories)/(general)/organizations/[groupid]/courses/[courseid]/[sectionid]/page.tsx
+++ src/app/(profile)/(categories)/(general)/organizations/[groupid]/courses/[courseid]/[sectionid]/page.tsx
@@ -3,17 +3,18 @@ import { onGetGroupInfo } from "@/actions/groups"
 import { CourseContentForm } from "@/components/forms/course-content"
 
 type Props = {
-    params: { sectionid: string; groupid: string }
+    params: Promise<{ sectionid: string; groupid: string }>
 }
 
 const CourseModuleSection = async ({ params }: Props) => {
+    const { groupid, sectionid } = await params;
     const user = await onAuthenticatedUser()
-    const group = await onGetGroupInfo(params.groupid)
+    const group = await onGetGroupInfo(groupid)
 
     return (
         <CourseContentForm
             groupid={group.group?.userId!}
-            sectionid={params.sectionid}
+            sectionid={sectionid}
             userid={user.id!}
         />
     )
