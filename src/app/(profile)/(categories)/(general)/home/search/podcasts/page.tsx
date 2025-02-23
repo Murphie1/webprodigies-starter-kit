da494b449eacda316af861e8de4c934efb279104
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-02-23 12:05:10 +0100

    Update page.tsx

--- src/app/(profile)/(categories)/(general)/home/search/podcasts/page.tsx
+++ src/app/(profile)/(categories)/(general)/home/search/podcasts/page.tsx
@@ -1,3 +1,53 @@
+"use client";
+
+import EmptyState from "@/components/EmptyState";
+import LoaderSpinner from "@/components/LoaderSpinner";
+import PodcastCard from "@/components/PodcastCard";
+import Searchbar from "@/components/Searchbar";
+import { api } from "~/convex/_generated/api";
+import { useQuery } from "convex/react";
+import React from "react";
+
+interface DiscoverProps {
+  searchParams: { [key: string]: string | undefined };
+}
+
+const Discover = ({ searchParams }: DiscoverProps) => {
+  const search = searchParams?.search || "";
+
+  const podcastsData = useQuery(api.podcasts.getPodcastBySearch, {
+    search,
+  });
+
+  return (
+    <div className="flex flex-col gap-9">
+      <Searchbar />
+      <div className="flex flex-col gap-9">
+        <h1 className="text-20 font-bold text-white-1">
+          {!search ? "Discover Trending Podcasts" : "Search results for "}
+          {search && <span className="text-white-2">{search}</span>}
+        </h1>
+        {podcastsData ? (
+          <>
+            {podcastsData.length > 0 ? (
+              <div className="podcast_grid">
+                {podcastsData.map(({ _id, podcastTitle, podcastDescription, imageUrl }) => (
+                  <PodcastCard key={_id} imgUrl={imageUrl!} title={podcastTitle} description={podcastDescription} podcastId={_id} />
+                ))}
+              </div>
+            ) : (
+              <EmptyState title="No results found" />
+            )}
+          </>
+        ) : (
+          <LoaderSpinner />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Discover;
 {/*"use client";
 
 import EmptyState from "@/components/EmptyState";
@@ -47,4 +97,4 @@ const Discover: React.FC<DiscoverProps> = ({ searchParams }) => {
   );
 };
 
-export default Discover;
+export default Discover;*/}
