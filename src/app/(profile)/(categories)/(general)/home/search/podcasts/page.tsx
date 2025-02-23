aaa958fe8fc44c9cfee5ffd4407dbcc8f933154c
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-02-23 22:10:01 +0100

    Update page.tsx

--- src/app/(profile)/(categories)/(general)/home/search/podcasts/page.tsx
+++ src/app/(profile)/(categories)/(general)/home/search/podcasts/page.tsx
@@ -1,22 +1,4 @@
-import LoaderSpinner from "@/components/LoaderSpinner";
-
-const Page = () => {
-  return (
-    <LoaderSpinner />
-    );
-};
-
-export default Page;
-
-{/*"use client";
-
-import EmptyState from "@/components/EmptyState";
-import LoaderSpinner from "@/components/LoaderSpinner";
-import PodcastCard from "@/components/PodcastCard";
-import Searchbar from "@/components/Searchbar";
-import { api } from "~/convex/_generated/api";
-import { useQuery } from "convex/react";
-import React from "react";
+import Body from "./_components/body";
 
 interface DiscoverProps {
   searchParams: Promise<{ [key: string]: string | undefined }>;
@@ -26,36 +8,9 @@ const Discover = async ({ searchParams }: DiscoverProps) => {
   const params = await searchParams;
   const search = params?.search || "";
 
-  const podcastsData = useQuery(api.podcasts.getPodcastBySearch, {
-    search,
-  });
-
   return (
-    <div className="flex flex-col gap-9">
-      <Searchbar />
-      <div className="flex flex-col gap-9">
-        <h1 className="text-20 font-bold text-white-1">
-          {!search ? "Discover Trending Podcasts" : "Search results for "}
-          {search && <span className="text-white-2">{search}</span>}
-        </h1>
-        {podcastsData ? (
-          <>
-            {podcastsData.length > 0 ? (
-              <div className="podcast_grid">
-                {podcastsData.map(({ _id, podcastTitle, podcastDescription, imageUrl }) => (
-                  <PodcastCard key={_id} imgUrl={imageUrl!} title={podcastTitle} description={podcastDescription} podcastId={_id} />
-                ))}
-              </div>
-            ) : (
-              <EmptyState title="No results found" />
-            )}
-          </>
-        ) : (
-          <LoaderSpinner />
-        )}
-      </div>
-    </div>
+    <Body search={search} />
   );
 };
 
-export default Discover;*/}
+export default Discover;
