cb8931587316242139ab0f8fef8601b4dd982db6
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-02-23 22:31:56 +0100

    Update page.tsx

--- src/app/(profile)/podcast/podcaster/[profileId]/page.tsx
+++ src/app/(profile)/podcast/podcaster/[profileId]/page.tsx
@@ -1,83 +1,20 @@
-"use client"
-
-import { useQuery } from "convex/react"
-
-import EmptyState from "@/components/EmptyState"
+import Body from "./_components/body"
 import LoaderSpinner from "@/components/LoaderSpinner"
-import PodcastCard from "@/components/PodcastCard"
-import ProfileCard from "@/components/ProfileCard"
-import { api } from "~/convex/_generated/api"
-import PodcastPlayer from "@/components/PodcastPlayer"
-import LeftSidebar from "@/components/LeftSidebar"
-import RightSidebar from "@/components/RightSidebar"
-import MobileNav from "@/components/MobileNav"
 
 const ProfilePage = ({
     params,
 }: {
-    params: {
+    params: Promise<{
         profileId: string
-    }
+    }>
 }) => {
-    const user = useQuery(api.users.getUserById, {
-        clerkId: params.profileId,
-    })
-    const podcastsData = useQuery(api.podcasts.getPodcastByAuthorId, {
-        authorId: params.profileId,
-    })
-
-    if (!user || !podcastsData) return <LoaderSpinner />
+    const { profileId } = await params
+    
+    if (!profileId) return <LoaderSpinner />
 
     return (
         <section className="mt-9 flex flex-col">
-            <div className="hidden md:flex">
-                <LeftSidebar />
-            </div>
-            <MobileNav />
-            <h1 className="text-20 font-bold text-white-1 max-md:text-center">
-                Podcaster Profile
-            </h1>
-            <div className="mt-6 flex flex-col gap-6 max-md:items-center md:flex-row">
-                {podcastsData && user ? (
-                    <ProfileCard
-                        podcastData={podcastsData!}
-                        imageUrl={user?.imageUrl!}
-                        userFirstName={user?.name!}
-                    />
-                ) : (
-                    <div />
-                )}
-            </div>
-            <section className="mt-9 flex flex-col gap-5">
-                <h1 className="text-20 font-bold text-white-1">All Podcasts</h1>
-                {podcastsData && podcastsData.podcasts.length > 0 ? (
-                    <div className="podcast_grid">
-                        {podcastsData?.podcasts
-                            ?.slice(0, 4)
-                            .map((podcast) => (
-                                <PodcastCard
-                                    key={podcast._id}
-                                    imgUrl={podcast.imageUrl!}
-                                    title={podcast.podcastTitle!}
-                                    description={podcast.podcastDescription}
-                                    podcastId={podcast._id}
-                                />
-                            ))}
-                    </div>
-                ) : (
-                    <EmptyState
-                        title="You have not created any podcasts yet"
-                        buttonLink="/podcast/create"
-                        buttonText="Create Podcast"
-                    />
-                )}
-            </section>
-            <div className="hidden md:flex">
-                <RightSidebar />
-            </div>
-            <PodcastPlayer />
+            <Body profileId={profileId} />
         </section>
-    )
-}
 
 export default ProfilePage
