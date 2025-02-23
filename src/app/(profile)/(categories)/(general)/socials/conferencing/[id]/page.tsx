05cd75ea9a9e276da0c22fdc75820b25426a7459
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-02-23 22:16:20 +0100

    Update page.tsx

--- src/app/(profile)/(categories)/(general)/socials/conferencing/[id]/page.tsx
+++ src/app/(profile)/(categories)/(general)/socials/conferencing/[id]/page.tsx
@@ -1,58 +1,16 @@
-'use client';
-
-import { useState } from 'react';
-import { useUser } from '@clerk/nextjs';
-import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
-//import { useParams } from 'next/navigation';
-import Loader  from '@/components/conference/Loader';
-
-import { useGetCallById } from '@/hooks/stream/useGetCallById';
-import Alert from '@/components/conference/Alert';
-import MeetingSetup from '@/components/conference/MeetingSetup';
-import MeetingRoom from '@/components/conference/MeetingRoom';
+import Body from "./_components/body";
 
 // Define the IParams interface
 type Props = {
-  params: {
+  params: Promise<{
   id: string;
+}>
 }
-}
 
-const MeetingPage = ({ params }: Props) => {
-  
-  const { isLoaded, user } = useUser();
-  const { call, isCallLoading } = useGetCallById(params.id);
-  const [isSetupComplete, setIsSetupComplete] = useState(false);
-
-  if (!isLoaded || isCallLoading) return <Loader />;
-
-  if (!call)
-    return (
-      <p className="text-center text-3xl font-bold">
-        Call Not Found
-      </p>
-    );
-
-  // Check if the user is allowed to join the call
-  const notAllowed =
-    call.type === 'invited' &&
-    (!user || !call.state.members.find((m) => m.user.id === user.id));
-
-  if (notAllowed)
-    return <Alert title="You are not allowed to join this meeting" />;
-
+const MeetingPage = async ({ params }: Props) => {
+  const { id } = await params;
   return (
-    <main className="h-screen pb-[75px] md:pb-2 w-full">
-      <StreamCall call={call}>
-        <StreamTheme>
-          {!isSetupComplete ? (
-            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
-          ) : (
-            <MeetingRoom />
-          )}
-        </StreamTheme>
-      </StreamCall>
-    </main>
+  <Body id={id} />
   );
 };
 
