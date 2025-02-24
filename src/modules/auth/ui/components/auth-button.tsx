6ef3645a918ce1b4cf7cce6f59a4b5519225719d
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-02-24 17:29:18 +0100

    Create auth-button.tsx

---
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ClapperboardIcon, UserCircleIcon } from "lucide-react";
+import { UserButton, SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
+
+export const AuthButton = () => {
+  return (
+    <>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant="outline"
+            className="px-4 py-2 text-sm font-medium text-blue-600 hover:bg-blue-500 border-blue-500/70 rounded-full shadow-none [&_svg]:size-4"
+          >
+            <UserCircleIcon />
+            Sign in
+          </Button>
+        </SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton>
+          <UserButton.MenuItems>
+            <UserButton.Link
+              label="Studio"
+              href="/studio"
+              labelIcon={<ClapperboardIcon className="size-4" />}
+            />
+          </UserButton.MenuItems>
+        </UserButton>
+      </SignedIn>
+    </>
+  );
+};
