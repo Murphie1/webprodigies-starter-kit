fa291e2387305899517abe1e1336f2ce47d6d651
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-02-24 16:59:31 +0100

    Create procedures.ts

---
+++ src/modules/videos/server/procedures.ts
@@ -0,0 +1,40 @@
+import { db } from "@/lib/db";
+import { videos } from "@/lib/db/schema";
+import { mux } from "@/lib/teacher/mux";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+
+export const videosRouter = createTRPCRouter({
+  create: protectedProcedure.mutation(async ({ ctx }) => {
+    const { id: userId } = ctx.user;
+
+    const upload = await mux.video.uploads.create({
+      new_asset_settings: {
+        passthrough: userId,
+        playback_policy: ["public"],
+        input: [
+          {
+            generated_subtitles: [
+              {
+                language_code: "en",
+                name: "English",
+              },
+            ],
+          },
+        ],
+      },
+      cors_origin: "*",
+    });
+
+    const [video] = await db
+      .insert(videos)
+      .values({
+        title: "title",
+        userId,
+        muxStatus: "waiting",
+        muxUploadId: upload.id,
+      })
+      .returning();
+
+    return { video, url: upload.url };
+  }),
+});
