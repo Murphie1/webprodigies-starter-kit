cb6cdd7f805fa7c5fa4c100e3e9db77bd4771291
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-02-24 16:55:40 +0100

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,17 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
+export function formatDuration(duration: number) {
+  const minutes = Math.floor(duration / 60000);
+  const seconds = Math.floor((duration % 60000) / 1000);
+  return `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}`;
+}
+
+export function snakeCaseToTitle(str: string) {
+  return str.replace(/_/g, " ").replace(/\b\w/g, (char) => char.toUpperCase());
+      }
 
 export const supabaseClient = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
