f013f58f0ca32917ecb88fd3366ed32fd324b04c
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-02-24 17:02:52 +0100

    Create videos-section.tsx

---
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import { trpc } from "@/trpc/client";
+import { DEFAULT_LIMIT } from "@/constants";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { InfinitScroll } from "@/components/teacher/videos/infinit-scroll";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Link from "next/link";
+import { VideoThumbnail } from "@/modules/videos/ui/components/video-thumbnail";
+import { snakeCaseToTitle } from "@/lib/utils";
+import { format } from "date-fns";
+import { Globe2Icon, LockIcon } from "lucide-react";
+
+export const VideosSection = () => {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Something went wrong</p>}>
+        <VideosSectionSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const VideosSectionSuspense = () => {
+  const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+    {
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+
+  return (
+    <div className="border-y">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="pl-6 w-[510px]">Video</TableHead>
+            <TableHead>Visibility</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Date</TableHead>
+            <TableHead className="text-right">Views</TableHead>
+            <TableHead className="text-right">Comments</TableHead>
+            <TableHead className="text-right pr-6">Likes</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {videos.pages
+            .flatMap((page) => page.items)
+            .map((video) => (
+              <Link
+                href={`/studio/videos/${video.id}`}
+                key={video.id}
+                legacyBehavior
+              >
+                <TableRow className="cursor-pointer">
+                  <TableCell>
+                    <div className="flex items-center gap-4">
+                      <div className="relative aspect-video w-36 shrink-0">
+                        <VideoThumbnail
+                          imageUrl={video.thumbnailUrl}
+                          previewUrl={video.previewUrl}
+                          title={video.title}
+                          duration={video.duration || 0}
+                        />
+                      </div>
+                      <div className="flex flex-col overflow-hidden gap-y-1">
+                        <span className="text-sm line-clamp-1">
+                          {video.title}
+                        </span>
+
+                        <span className="text-xs text-muted-foreground line-clamp-1">
+                          {video.description || "No description"}
+                        </span>
+                      </div>
+                    </div>
+                  </TableCell>
+                  <TableCell>
+                    <div className="flex items-center">
+                      {video.visibility === "private" ? (
+                        <LockIcon className="size-4 mr-2" />
+                      ) : (
+                        <Globe2Icon className="size-4 mr-2" />
+                      )}
+                      {snakeCaseToTitle(video.visibility)}
+                    </div>
+                  </TableCell>
+                  <TableCell>
+                    <div className="flex items-center">
+                      {snakeCaseToTitle(video.muxStatus || "error")}
+                    </div>
+                  </TableCell>
+                  <TableCell className="text-xs truncate">
+                    {format(new Date(video.createdAt), "d MMM, yyyy")}
+                  </TableCell>
+                  <TableCell className="text-right">views</TableCell>
+                  <TableCell className="text-right">comments</TableCell>
+                  <TableCell className="text-right pr-6">likes</TableCell>
+                </TableRow>
+              </Link>
+            ))}
+        </TableBody>
+      </Table>
+
+      <InfinitScroll
+        hasNextPage={query.hasNextPage}
+        fetchNextPage={query.fetchNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+      />
+    </div>
+  );
+};
+
+export default VideosSectionSuspense;
