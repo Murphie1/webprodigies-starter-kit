723058d8b52d6aa89d614833414b2511b6d0377a
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-02-24 16:10:04 +0100

    Update page.tsx

--- src/app/(profile)/podcast/[podcastId]/page.tsx
+++ src/app/(profile)/podcast/[podcastId]/page.tsx
@@ -3,11 +3,13 @@ import { Id } from "~/convex/_generated/dataModel"
 import LoaderSpinner from "@/components/LoaderSpinner"
 import React from "react"
 
+type Props = {
+    params: Promise<{ podcastId: string}>
+}
+
 const PodcastDetails = async ({
-    params: { podcastId },
-}: {
-    params: Promise<{ podcastId: Id<"podcasts"> }>
-}) => {
+    params,
+}: Props) => {
     const { podcastId } = await params
     if (!podcast) return <LoaderSpinner />
 
